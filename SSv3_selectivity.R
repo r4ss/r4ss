64e3b93ab59082024ba798e598ad884281c26e3b
Ian.Taylor@noaa.gov <Ian.Taylor@noaa.gov@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2009-03-31 22:39:44 +0000

    added some notes to the code and removed automatic call to function

--- SSv3_selectivity.R
+++ SSv3_selectivity.R
@@ -1,18 +1,30 @@
-rm(list=ls(all=TRUE))
-
 lines.sel <- function(x, model, sp, min.dist, max.dist, ...) {
- 
+
+################################################################################
+#
+# lines.sel        March 31, 2009.
+# This function comes with no warranty or guarantee of accuracy
+#
+# Purpose: Plot selectivity function for Stock Synthesis.
+# Written: Tommy Garrison, UW
+# Returns: plot of double normal or double logistic selectivity
+# General: parameterization matched Stock Synthesis v.3
+# Notes:   For documentation go to: http://code.google.com/p/r4ss/wiki/Documentation
+# Required packages: none
+#
+################################################################################
+
 		if(model == "Double_Logistic") {
 		sel <- function(x) {
-     			t1 <- min.dist+(1/(1.+exp(-sp[3])))*(sp[1]-min.dist)   
-			t1min <- 1/(1+exp(-exp(sp[4])*(min.dist-t1)))*0.9999  
-			t1max <- 1/(1.+exp(-exp(sp[4])*(sp[1]-t1)))*1.0001  
-			t1power <- log(0.5)/log((0.5-t1min)/(t1max-t1min))  
-			t2 <- (sp[1]+sp[8])+(1/(1+exp(-sp[6])))*(max.dist-(sp[1]+sp[8]))    
-			t2min <- 1/(1+exp(-exp(sp[7])*(sp[1]+sp[8]-t2)))*0.9999 
-			t2max <- 1/(1+exp(-exp(sp[7])*(max.dist-t2)))*1.0001  
+     			t1 <- min.dist+(1/(1.+exp(-sp[3])))*(sp[1]-min.dist)
+			t1min <- 1/(1+exp(-exp(sp[4])*(min.dist-t1)))*0.9999
+			t1max <- 1/(1.+exp(-exp(sp[4])*(sp[1]-t1)))*1.0001
+			t1power <- log(0.5)/log((0.5-t1min)/(t1max-t1min))
+			t2 <- (sp[1]+sp[8])+(1/(1+exp(-sp[6])))*(max.dist-(sp[1]+sp[8]))
+			t2min <- 1/(1+exp(-exp(sp[7])*(sp[1]+sp[8]-t2)))*0.9999
+			t2max <- 1/(1+exp(-exp(sp[7])*(max.dist-t2)))*1.0001
 			t2power <- log(0.5)/log((0.5-t2min)/(t2max-t2min))
-			final <- 1/(1+exp(-sp[5])) 
+			final <- 1/(1+exp(-sp[5]))
 			join1 <- 1/(1+exp(10.*(x-sp[1])))
 			join2 <- 1/(1+exp(10.*(x-(sp[1]+sp[8]))))
 			join3 <- 1/(1+exp(10.*(x-max.dist)))
@@ -33,13 +45,13 @@ lines.sel <- function(x, model, sp, min.dist, max.dist, ...) {
 			final <- sp[6]
 
            	 	if(sp[5] < -1000) {
-				j1 <-  -1001 - round(sp[5])     
+				j1 <-  -1001 - round(sp[5])
 				sel[1:j1] <- 1.0e-06
             	}
             	if(sp[5] >= -1000) {
             		j1 <- startbin - 1
               			if(sp[5] > -999) {
-              				point1 <- 1.0/(1.0+exp(-sp[5])) 
+              				point1 <- 1.0/(1.0+exp(-sp[5]))
               				t1min <- exp(-(x[startbin]-peak)^2 / upselex)
               			}
             	}
@@ -59,25 +71,39 @@ lines.sel <- function(x, model, sp, min.dist, max.dist, ...) {
 			if(sp[6] > -999) dsc <- 1.0 + (point2 - 1.0) * (exp(-t2^2 / downselex)-1.0) / (t2min-1.0)
             	if(sp[6] <= -999) dsc <- exp(-(t2)^2/downselex)
             	sel[(j1+1):j2] <- asc*(1.0-join1)+join1*(1.0-join2+dsc*join2)
-       
+
 			if(startbin > 1 && sp[5] >= -1000) {
 				sel[1:startbin] <- (x[1:startbin] / x[startbin])^2 * sel[startbin]
 			}
-            
+
             	if(j2 < length(x)) sel[j2+1:nlength] <- sel[j2]
 			return(sel)
 		}}
-	
+
 	if(model == "Double_Normal") col <- "blue"
-	if(model == "Double_Logistic") col <- "red"	
+	if(model == "Double_Logistic") col <- "red"
 
 	curve(sel, add=T, from=c(min.dist, max.dist), type='l', lwd=1, col=col)
 
-}
+} # end lines.sel function
 
-selfit <- 
-function (minLength, maxLength, silent = FALSE) 
+selfit <- function (minLength=10, maxLength=65, silent = FALSE)
 {
+
+################################################################################
+#
+# selfit   March 31, 2009.
+# This function comes with no warranty or guarantee of accuracy
+#
+# Purpose: Provide GUI for the plot function, lines.sel
+# Written: Tommy Garrison, UW
+# Returns: plots double normal or double logistic selectivity
+# General: parameterization matched Stock Synthesis v.3
+# Notes:   For documentation go to: http://code.google.com/p/r4ss/wiki/Documentation
+# Required packages: none
+#
+################################################################################
+
     require(tcltk) || stop("package tcltk is required")
     geterrmessage()
     done <- tclVar(0)
@@ -108,21 +134,21 @@ function (minLength, maxLength, silent = FALSE)
         p6 <- as.numeric(tclObj(sp6))
         p7 <- as.numeric(tclObj(sp7))
         p8 <- as.numeric(tclObj(sp8))
-        
+
         plot(seq(2, maxLength, l=2), seq(0,1,l=2), type='n', xlab="length bin", ylab="selectivity")
 
         fit <- get("selfit.tmp", envir = selfit.env)
 
         lapply(fit, function(x) lines.sel(seq(minL, maxL, by=2), model = x$model, sp = x$sp, min.dist = x$min.dist, max.dist = x$max.dist))
-        
+
         if (k == "Double_Logistic") {
-            lines.sel(x = seq(minL, maxL, by=2), model = k, 
-                sp = c(p1,p2,p3,p4,p5,p6,p7,p8), 
+            lines.sel(x = seq(minL, maxL, by=2), model = k,
+                sp = c(p1,p2,p3,p4,p5,p6,p7,p8),
                 min.dist = minL, max.dist = maxL)
         }
         if (k == "Double_Normal") {
-            lines.sel(x = seq(minL, maxL, by=2), model = k, 
-                sp = c(p1,p2,p3,p4,p5,p6), 
+            lines.sel(x = seq(minL, maxL, by=2), model = k,
+                sp = c(p1,p2,p3,p4,p5,p6),
                 min.dist = minL, max.dist = maxL)
         }
     }
@@ -144,7 +170,7 @@ function (minLength, maxLength, silent = FALSE)
             tkconfigure(entry.sp8, state = "normal")
             tkconfigure(ts9, state = "normal")
         }
-		
+
         replot()
     }
 
@@ -158,17 +184,17 @@ function (minLength, maxLength, silent = FALSE)
     tkpack(tklabel(frame0, text = "Length Parameters"), fill = "both", side = "top")
 
     entry.minLB <- tkentry(frame0, textvariable = minLB, width="8")
-    tkpack(ts0 <- tkscale(frame0, label = "Min Length Bin", command = replot, 
-        from = 0, to = minLength, showvalue = 1, variable = minLB, 
-        resolution = 1, orient = "horiz", relief = "groove"), 
+    tkpack(ts0 <- tkscale(frame0, label = "Min Length Bin", command = replot,
+        from = 0, to = minLength, showvalue = 1, variable = minLB,
+        resolution = 1, orient = "horiz", relief = "groove"),
         fill = "x", expand = 1, padx = 3, ipadx = 30, pady = 2, ipady = 2, side = "left")
     tkpack(entry.minLB,  side = "right")
 
     frame1 <- tkframe(left.frm, relief = "groove", borderwidth = 2)
     entry.maxLB <- tkentry(frame1, textvariable = maxLB, width="8")
-    tkpack(ts1 <- tkscale(frame1, label = "Max Length Bin", command = replot, 
-        from = 0, to = maxLength, showvalue = 1, variable = maxLB, 
-        resolution = 1, orient = "horiz", relief = "groove"), 
+    tkpack(ts1 <- tkscale(frame1, label = "Max Length Bin", command = replot,
+        from = 0, to = maxLength, showvalue = 1, variable = maxLB,
+        resolution = 1, orient = "horiz", relief = "groove"),
         fill = "x", expand = 1, padx = 3, ipadx = 30, pady = 2, ipady = 2, side = "left")
     tkpack(entry.maxLB,  side = "right")
 
@@ -184,10 +210,10 @@ function (minLength, maxLength, silent = FALSE)
 
     frame4 <- tkframe(left.frm, relief = "groove", borderwidth = 2)
     entry.sp2 <- tkentry(frame4, textvariable = sp2, width="8")
-    tkpack(ts3 <- tkscale(frame4, label = "Parameter 2 :", command = replot, 
-        from = -5, to = 3, showvalue = 1, variable = sp2, 
-        resolution = 0.01, orient = "horiz", relief = "groove"), 
-        fill = "x", expand = 1, padx = 3, ipadx = 30, pady = 2, ipady = 2, side = "left")     
+    tkpack(ts3 <- tkscale(frame4, label = "Parameter 2 :", command = replot,
+        from = -5, to = 3, showvalue = 1, variable = sp2,
+        resolution = 0.01, orient = "horiz", relief = "groove"),
+        fill = "x", expand = 1, padx = 3, ipadx = 30, pady = 2, ipady = 2, side = "left")
     tkpack(entry.sp2, side = "right")
 
     frame5 <- tkframe(left.frm, relief = "groove", borderwidth = 2)
@@ -224,16 +250,16 @@ function (minLength, maxLength, silent = FALSE)
 
     frame9 <- tkframe(left.frm, relief = "groove", borderwidth = 2)
     entry.sp7 <- tkentry(frame9, textvariable = sp7, state = "disabled", width="8")
-    tkpack(ts8 <- tkscale(frame9, label = "Parameter 7 :", command = replot, 
-        from = -10, to = 10, showvalue = 1, variable = sp7, 
+    tkpack(ts8 <- tkscale(frame9, label = "Parameter 7 :", command = replot,
+        from = -10, to = 10, showvalue = 1, variable = sp7,
         state = "disabled", resolution = 0.01, orient = "horiz", relief = "groove"),
         fill = "x", expand = 1, padx = 3, ipadx = 30, pady = 2, ipady = 2, side = "left")
     tkpack(entry.sp7, side = "right")
 
     frame10 <- tkframe(left.frm, relief = "groove", borderwidth = 2)
     entry.sp8 <- tkentry(frame10, textvariable = sp8, state = "disabled", width="8")
-    tkpack(ts9 <- tkscale(frame10, label = "Parameter 8 :", command = replot, 
-        from = 0, to = 20, showvalue = 1, variable = sp8, 
+    tkpack(ts9 <- tkscale(frame10, label = "Parameter 8 :", command = replot,
+        from = 0, to = 20, showvalue = 1, variable = sp8,
         state = "disabled", resolution = 0.01, orient = "horiz", relief = "groove"),
         fill = "x", expand = 1, padx = 3, ipadx = 30, pady = 2, ipady = 2, side = "left")
     tkpack(entry.sp8, side = "right")
@@ -241,7 +267,7 @@ function (minLength, maxLength, silent = FALSE)
     frame2 <- tkframe(right.frm, relief = "groove", borderwidth = 2)
     tkpack(tklabel(frame2, text = "Function"))
     for (i in c("Double_Normal", "Double_Logistic")) {
-        tmp <- tkradiobutton(frame2, command = redraw, text = i, 
+        tmp <- tkradiobutton(frame2, command = redraw, text = i,
             value = i, variable = kernel)
         tkpack(tmp, anchor = "w")
     }
@@ -309,6 +335,5 @@ function (minLength, maxLength, silent = FALSE)
         return(fit)
     }
     else return(invisible())
-}
+} # end selfit function
 
-selfit(10,65)
