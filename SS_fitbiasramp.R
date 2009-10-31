4fa1186dfdbc89e91da606b585d35ff4b8441b14
Ian.Taylor@noaa.gov <Ian.Taylor@noaa.gov@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2009-10-31 00:39:52 +0000

    added new file with function for estimating the bias adjustment inputs

---
+++ SS_fitbiasramp.R
@@ -0,0 +1,111 @@
+# source('y:/h_itaylor/SS/maxbias/fitbiasramp.R')
+FitBiasRamp <- function(replist){
+  ##################
+  # function to estimate bias adjustment ramp
+  # for Stock Synthesis v3.04b
+  # by Ian Taylor
+  # October 30, 2009
+  ##################
+  if(!is.list(replist) | substr(replist$SS_version,1,8)!="SS-V3.04"){
+    print("!error: this function needs an input object created by SSv3_output from a SSv3.04 model")
+    return()
+  }
+  if(replist$inputs$covar==F){
+    print("!error, you need to have covar=T in the input to the SSv3_output function",quote=F)
+    return()
+  }
+  parameters <- replist$parameters
+  startyr    <- replist$startyr
+  recruit    <- replist$recruit
+  sigma_R_in <- replist$sigma_R_in
+
+  biasadjfit <- function(pars,yr,std,sigmaR){
+    biasadj <- biasadjfun(yr,pars)$biasadj
+    compare <- 1 - (std/sigmaR)^2
+    fit <- sum((biasadj-compare)^2)
+    return(fit)
+  }
+
+  getrecdevs <- function(replist){
+    parmat <- replist$parameters
+    rowrange <- (grep("SR_autocorr",parmat$Label)+1):(grep("InitF_1fishery1",parmat$Label)-1)
+    val <- parmat$Value[rowrange]
+    std <- parmat$Parm_StDev[rowrange]
+    return(data.frame(val=val,std=std))
+  }
+
+  optimfun <- function(yr,std){
+    pars=c(1950,1970,1995,1999,0.7)
+    biasopt <- optim(par=pars,fn=biasadjfit,yr=yr,std=std,
+                     sigmaR=sigma_R_in,method="L-BFGS-B")
+    return(biasopt)
+  }
+
+  biasadjfun <- function(yr,vec){
+    last_no     <- vec[1]
+    first_full  <- vec[2]
+    last_full   <- vec[3]
+    first_no    <- vec[4]
+    max_biasadj <- vec[5]
+    
+    biasadj <- rep(NA,length(yr))
+    for(i in 1:length(yr)){
+      y <- yr[i]
+      
+      if(y<=last_no){
+        biasadj[i]=0.;
+      }else{
+        if(y<=first_full){
+          biasadj[i] <- max_biasadj*(y-last_no)/(first_full-last_no);
+        }else{
+          if(y<=last_full){
+            biasadj[i]=max_biasadj;
+          }else{
+            if(y<=first_no){
+              biasadj[i]=max_biasadj*(1 - (y-last_full)/(first_no-last_full));
+            }else{
+              biasadj[i]=0.;
+            }}}}}
+    return(data.frame(yr=yr,biasadj=biasadj))
+  }
+  
+  recdevs <- getrecdevs(replist)
+  val <- recdevs$val
+  std <- recdevs$std
+
+  if(max(val)==0 | length(val)==0){
+    if(verbose) print("no rec devs estimated in this model",quote=F)
+    return()
+  }else{
+    
+    recdev_hi <- val + 1.96*std
+    recdev_lo <- val - 1.96*std
+    Yr <- recruit$year
+    ylim <- range(recdev_hi,recdev_lo)
+
+    par(mfrow=c(2,1),mar=c(2,5,1,1),oma=c(3,0,0,0))
+    plot(Yr,Yr,type='n',xlab="Year",
+         ylab='Recruitment deviation',ylim=ylim)
+    abline(h=0,col="grey")
+    arrows(Yr,recdev_lo,Yr,recdev_hi,length=0.03,code=3,angle=90,lwd=1.2)
+    points(Yr,val,pch=16)
+  }
+
+  print('estimating...',quote=F)
+  newbias <- optimfun(yr=Yr,std=std)
+  
+  yvals <- 1-(std/sigma_R_in)^2
+  plot(Yr,yvals,xlab="Year",
+       ylab=expression(1 - italic(SE(hat(R[i]))^2 / sigma[R])^2),
+       ylim=range(0,1,1.1*yvals),type="b",yaxs='i')
+  abline(h=0,col="grey")
+  abline(h=1,col="grey")
+
+  # bias correction (2nd axis, scaled by ymax)
+  lines(biasadjfun(Yr,newbias[[1]]),col=4,lwd=3,lty=1)
+  lines(recruit$year,recruit$biasadj,col=2,lwd=3,lty=2)
+  legend('topleft',col=c(2,4),lwd=3,lty=2:1,inset=.01,cex=.9,bg=rgb(1,1,1,.8),box.col=NA,
+         leg=c('bias adjust in model','estimated alternative'))
+  mtext(side=1,line=3,'Year')
+  return(newbias[[1]])
+}
