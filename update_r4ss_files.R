603cbe7b1efa9285ee9f528cff324fe6b991cabf
ian.taylor@noaa.gov <ian.taylor@noaa.gov@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2010-04-21 17:15:18 +0000

    updated update_r4ss_files.R

--- update_r4ss_files.R
+++ update_r4ss_files.R
@@ -17,6 +17,7 @@ update_r4ss_files <- function(trunk=T,branches=F){
   if(branches){
       getfilenames("http://r4ss.googlecode.com/svn/branches/input_file_objects")
       getfilenames("http://r4ss.googlecode.com/svn/branches/individual_plot_functions")
+      getfilenames("http://r4ss.googlecode.com/svn/branches/simulations")
   }
 
   print("update complete.",quote=F)
