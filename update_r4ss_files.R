d52a805d1b28a1b7cedc8126d75f5c45bf3f5d53
Ian.Taylor@noaa.gov <Ian.Taylor@noaa.gov@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2011-03-03 01:54:01 +0000

    updated updater function to safe files to local directory

--- update_r4ss_files.R
+++ update_r4ss_files.R
@@ -1,41 +1,61 @@
-update_r4ss_files <- function(local=NULL){
+update_r4ss_files <- function(local=NULL,save=FALSE){
   # read and parse the HTML file that will list all the file names
-  getfilenames <- function(webdir){
+  webdir <- "http://r4ss.googlecode.com/svn/trunk/"
+
+  getwebnames <- function(){
     changes <- readLines('http://code.google.com/p/r4ss/source/list')
     line <- changes[grep("detail?",changes)[6]]
-    cat("sourcing updated functions from",webdir,"\n")
+    cat("getting file names from",webdir,"\n")
     cat("most recent change:",strsplit(strsplit(line,">")[[1]][3],"<")[[1]][1],"\n")
 
     lines <- readLines(webdir,warn=F)
     filenames <- lines[grep('"*.R"',lines)]
     for(i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i],"\">")[[1]][2]   # split along: ">
     for(i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i],"</a>")[[1]][1] # split along: </a>
+    return(filenames)
+  }
 
+  getwebfiles <- function(filenames){
     # getting the files
     n <- length(filenames)
-    cat(n,"files found:\n")
+    cat(n,"files found in",webdir,"\n")
+    if(save) cat("saving all files to",local,"\n")
     for(i in 1:n){
-      cat("  sourcing ",filenames[i],"\n",sep="")
-      source(paste(webdir,filenames[i],sep="/"))
+      webfile <- paste(webdir,filenames[i],sep="/")
+      if(save){
+        localfile <- paste(local,filenames[i],sep="/")
+        temp <- readLines(webfile)
+        writeLines(temp,localfile)
+        cat("  writing ",localfile,"\n",sep="")
+      }else{
+        cat("  sourcing ",filenames[i],"\n",sep="")
+        source(webfile)
+      }
       flush.console()
     }
   }
 
-  getlocalfiles <- function(localdir){
-    filenames <- dir(localdir,pattern="*.R$")
+  getlocalfiles <- function(local){
+    filenames <- dir(local,pattern="*.R$")
     n <- length(filenames)
-    cat(n,"files found:\n")
+    cat(n,"files found in",local,"\n")
     for(i in 1:n){
       cat("  sourcing ",filenames[i],"\n",sep="")
-      source(paste(localdir,filenames[i],sep="/"))
+      source(paste(local,filenames[i],sep="/"))
       flush.console()
     }
   }
 
   if(is.null(local)){
-    getfilenames("http://r4ss.googlecode.com/svn/trunk/")
+    filenames <- getwebnames()
+    getwebfiles(filenames)
   }else{
+    if(save){
+      filenames <- getwebnames()
+      getwebfiles(filenames)
+    }
     getlocalfiles(local)
   }
   cat("update complete.\n")
 }
+
