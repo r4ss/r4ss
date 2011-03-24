ce8bf6e1c6825b5107378a87886678fe186f7a38
ian.taylor@noaa.gov <ian.taylor@noaa.gov@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2011-03-24 18:36:19 +0000

    added option for update_r4ss_files.R to get older version of the code

--- update_r4ss_files.R
+++ update_r4ss_files.R
@@ -1,24 +1,37 @@
-update_r4ss_files <- function(local=NULL,save=FALSE){
+update_r4ss_files <- function(local=NULL,save=FALSE,revision="newest"){
   # read and parse the HTML file that will list all the file names
-  webdir <- "http://r4ss.googlecode.com/svn/trunk/"
-
   getwebnames <- function(){
     changes <- readLines('http://code.google.com/p/r4ss/source/list')
     line <- changes[grep("detail?",changes)[6]]
-    cat("getting file names from",webdir,"\n")
     cat("most recent change:",strsplit(strsplit(line,">")[[1]][3],"<")[[1]][1],"\n")
+    current_revision <- as.numeric(strsplit(strsplit(line,"detail?r=",fixed=TRUE)[[1]][2],"\">")[[1]][1])
+    cat("current revision number:",current_revision,"\n")
 
+
+    if(revision=="newest"){
+      webdir <- "http://r4ss.googlecode.com/svn/trunk/"
+    }else{
+      if(is.numeric(revision) && revision<=current_revision){
+        webdir <- paste("http://r4ss.googlecode.com/svn-history/r",revision,"/trunk/",sep="")
+      }else{
+        stop("'revision' input should either be 'newest', or an integer <",current_revision)
+      }
+    }
+
+    cat("getting file names from",webdir,"\n")
     lines <- readLines(webdir,warn=F)
     filenames <- lines[grep('"*.R"',lines)]
     for(i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i],"\">")[[1]][2]   # split along: ">
     for(i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i],"</a>")[[1]][1] # split along: </a>
-    return(filenames)
+    return(list(filenames=filenames, webdir=webdir))
   }
 
-  getwebfiles <- function(filenames){
+  getwebfiles <- function(fileinfo){
+    filenames <- fileinfo$filenames
+    webdir <- fileinfo$webdir
     # getting the files
     n <- length(filenames)
-    cat(n,"files found in",webdir,"\n")
+    cat(n,"files found\n")
     if(save) cat("saving all files to",local,"\n")
     for(i in 1:n){
       webfile <- paste(webdir,filenames[i],sep="/")
@@ -47,12 +60,12 @@ update_r4ss_files <- function(local=NULL,save=FALSE){
   }
 
   if(is.null(local)){
-    filenames <- getwebnames()
-    getwebfiles(filenames)
+    fileinfo <- getwebnames()
+    getwebfiles(fileinfo)
   }else{
     if(save){
-      filenames <- getwebnames()
-      getwebfiles(filenames)
+      fileinfo <- getwebnames()
+      getwebfiles(fileinfo)
     }
     getlocalfiles(local)
   }
