cf490ddc02f54704a9b86843c567df630bdefdbb
ian.taylor@noaa.gov <ian.taylor@noaa.gov@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2011-05-06 20:48:35 +0000

    added color to plotCI.R

--- plotCI.R
+++ plotCI.R
@@ -1,22 +1,29 @@
-plotCI <- function(x,y=NULL,uiw,liw=uiw,ylo=NULL,yhi=NULL,...,
-                   sfrac = 0.01,ymax=NULL,add=FALSE)
+plotCI <-
+function (x, z = x, y = NULL, uiw, liw = uiw, ylo = NULL, yhi = NULL,
+          ..., sfrac = 0.01, ymax = NULL, add = FALSE, col = "black")
 {
-  # Written by Venables; modified for access to ylim and contents
-  if(is.list(x))
-  { y <- x$y
-    x <- x$x}
-  if(is.null(y))
-  { if(is.null(x)){stop("both x and y NULL")}
+  # Written by Venables; modified for access to ylim and contents, and color
+  if (is.list(x)) {
+    y <- x$y
+    x <- x$x
+  }
+  if (is.null(y)) {
+    if (is.null(x)) {
+      stop("both x and y NULL")
+    }
     y <- as.numeric(x)
-    x <- seq(along = x)}
+    x <- seq(along = x)
+  }
   ui <- y + uiw
   li <- y - liw
-  ylim <- range(c(y,ui,li,ylo,yhi,ymax))
-  if(!add) plot(x,y,ylim=ylim,...) else points(x,y,...)  
+  ylim <- range(c(y, ui, li, ylo, yhi, ymax))
+  if (!add)
+    plot(x, y, ylim = ylim, col= col, ...)
+  else points(x, y, ...)
   smidge <- diff(par("usr")[1:2]) * sfrac
-  segments(x,li,x,ui)
+  segments(x, li, x, ui, col= col)
   x2 <- c(x, x)
   ul <- c(li, ui)
-  segments(x2 - smidge, ul, x2 + smidge, ul)
+  segments(x2 - smidge, ul, x2 + smidge, ul, col= col)
   invisible(list(x = x, y = y))
 }
