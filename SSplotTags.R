79fc2d42bdbc3a9ec8175be8705cb720678f2b7b
iantaylornmfs <iantaylornmfs@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2011-09-29 23:53:28 +0000

    one more update to SSplotTags.R

--- SSplotTags.R
+++ SSplotTags.R
@@ -55,6 +55,7 @@ SSplotTags <-
           polygon(xx,yy,col=ifelse(iy<=latency,col3,col4))
         }
         points(tagtemp$Yr,tagtemp$Exp,type="o",lty=1,pch=16)
+        if(latency>0) points(tagtemp$Yr[1:latency],tagtemp$Exp[1:latency],type="o",lty=1,pch=21,bg="white")
 
         # add labels in left and lower outer margins once per page
         mfg <- par("mfg")
