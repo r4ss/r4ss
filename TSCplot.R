e9f7939233f5913aab6d73034e560f7b828b57c1
allan.hicks.NMFS@gmail.com <allan.hicks.NMFS@gmail.com@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2012-03-20 18:26:10 +0000

    Used the correct virgin spawnign biomass to calculate depletion.

--- TSCplot.R
+++ TSCplot.R
@@ -18,6 +18,7 @@ TSCplot <- function(SSout,
 
     if(!is.null(makePDF) & !is.null(makePNG)) stop("Cannot specify both makePDF and makePNG. Choose only one.\n")
 
+    indVirgin <- which(SSout$timeseries$Era=="VIRG")
     ind <- which(SSout$timeseries$Era=="TIME")
     ind <- c(ind,max(ind)+1)
     tmp <- SSout$timeseries[ind,]
@@ -37,7 +38,7 @@ TSCplot <- function(SSout,
     par(new=T)
     plot(SP$Yr, SP$SpawnBio/SP$SpawnBio[1], yaxt='n', yaxs='i', xaxt = 'n', ylab="", xlab="",
            ylim=ylimDepl, type='l', lwd=lwdDepl,  cex.axis=cex.axis)
-    points(SP$Yr[ind], SP$SpawnBio[ind]/SP$SpawnBio[1], pch=pchDepl, col=colDepl)
+    points(SP$Yr[ind], SP$SpawnBio[ind]/SSout$timeseries$SpawnBio[indVirgin], pch=pchDepl, col=colDepl)
     axis(4, at=seq(0, 1, 0.1), cex.axis=cex.axis)
     mtext(c("Year","Total mortality catch (mt)", "Depletion"), side=c(1,2,4), line=labelLines, cex=1.5)
 
