dc6668d23af0629624907c06ed90a092ed8a4509
allan.hicks.NMFS@gmail.com <allan.hicks.NMFS@gmail.com@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2012-09-22 19:42:49 +0000

    Added argument for y label in SSplotSummaryF

--- SSplotSummaryF.R
+++ SSplotSummaryF.R
@@ -1,4 +1,4 @@
-SSplotSummaryF <- function(replist,yrs="all",Ftgt=NA) {
+SSplotSummaryF <- function(replist,yrs="all",Ftgt=NA,ylab="Summary Fishing Mortality") {
     #plots the summary F (or harvest rate) as set up in the starter file
     #needs a lot of work to be generalized
     if(yrs=="all") {yrs <- replist$startyr:replist$endyr}
@@ -6,7 +6,7 @@ SSplotSummaryF <- function(replist,yrs="all",Ftgt=NA) {
     uppFtot <- Ftot$Value + 1.96*Ftot$StdDev
     lowFtot <- Ftot$Value - 1.96*Ftot$StdDev
     Fmax <- max(c(uppFtot,Ftgt+0.01),na.rm=T)
-    plot(as.numeric(substring(Ftot$LABEL,3,6)),Ftot$Value,pch=16,type="p",xlab="Year",ylab="Summary harvest rate",ylim=c(0,Fmax),cex.lab=1.0,cex.axis=1.0,cex=0.7)
+    plot(as.numeric(substring(Ftot$LABEL,3,6)),Ftot$Value,pch=16,type="p",xlab="Year",ylab=ylab,ylim=c(0,Fmax),cex.lab=1.0,cex.axis=1.0,cex=0.7)
     abline(h=Ftgt,col="red")
     segments(as.numeric(substring(Ftot$LABEL,3,6)),uppFtot,as.numeric(substring(Ftot$LABEL,3,6)),lowFtot,col=gray(0.5))
 }
