10709e8c29c95483dd815a7e0575708264961187
allan.hicks.NMFS@gmail.com <allan.hicks.NMFS@gmail.com@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2012-09-22 19:36:22 +0000

    Added the function SSplotSummaryF which plots the summary F from the derived_quants list which are the F's (or harvest rate) as specified in the starter file.

---
+++ SSplotSummaryF.R
@@ -0,0 +1,12 @@
+SSplotSummaryF <- function(replist,yrs="all",Ftgt=NA) {
+    #plots the summary F (or harvest rate) as set up in the starter file
+    #needs a lot of work to be generalized
+    if(yrs=="all") {yrs <- replist$startyr:replist$endyr}
+    Ftot <- replist$derived_quants[match(paste("F_",yrs,sep=""),replist$derived_quants$LABEL),]
+    uppFtot <- Ftot$Value + 1.96*Ftot$StdDev
+    lowFtot <- Ftot$Value - 1.96*Ftot$StdDev
+    Fmax <- max(c(uppFtot,Ftgt+0.01),na.rm=T)
+    plot(as.numeric(substring(Ftot$LABEL,3,6)),Ftot$Value,pch=16,type="p",xlab="Year",ylab="Summary harvest rate",ylim=c(0,Fmax),cex.lab=1.0,cex.axis=1.0,cex=0.7)
+    abline(h=Ftgt,col="red")
+    segments(as.numeric(substring(Ftot$LABEL,3,6)),uppFtot,as.numeric(substring(Ftot$LABEL,3,6)),lowFtot,col=gray(0.5))
+}
