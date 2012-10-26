58b0e9cd27d523ecc6da8215627f4c7fcb0b60ab
iantaylornmfs <iantaylornmfs@7b6603f8-9a1c-11dd-ad9f-f77439c74b79>
2012-10-26 00:20:02 +0000

    Updated updater from Allan Hicks that sends sourced files to environment other than .GlobalEnv. This finally solves Issue 53.

--- update_r4ss_files.R
+++ update_r4ss_files.R
@@ -1,72 +1,87 @@
-update_r4ss_files <- function(local=NULL,save=FALSE,revision="newest"){
-  # read and parse the HTML file that will list all the file names
-  getwebnames <- function(){
-    changes <- readLines('http://code.google.com/p/r4ss/source/list')
-    line <- changes[grep("detail?",changes)[6]]
-    cat("most recent change:",strsplit(strsplit(line,">")[[1]][3],"<")[[1]][1],"\n")
-    current_revision <- as.numeric(strsplit(strsplit(line,"detail?r=",fixed=TRUE)[[1]][2],"\">")[[1]][1])
-    cat("current revision number:",current_revision,"\n")
+update_r4ss_files <- function (local = NULL, save = FALSE,
+                               revision = "newest",env="updated_r4ss_files",pos=2){
 
-
-    if(revision=="newest"){
+  #if env is an environment simply wirte to that. If not, create a new environment
+  if(!is.environment(env)) {  
+    if(!is.character(env))
+      stop("'env' must be an environment or a character vector.",
+           "Use default if not sure to assign objects into global workspace\n")
+    if(is.character(env)) {
+      cat('Creating environment "',env,'", to store updated r4ss files.\n',sep="")
+      cat('To put the files in the global environment, set "env=.GlobalEnv"\n')
+      env <- attach(NULL,pos=pos,name=env)
+    }
+  }
+  getwebnames <- function() {
+    changes <- readLines("http://code.google.com/p/r4ss/source/list")
+    line <- changes[grep("detail?", changes)[6]]
+    cat("most recent change:", strsplit(strsplit(line, ">")[[1]][3], 
+                                        "<")[[1]][1], "\n")
+    current_revision <- as.numeric(strsplit(strsplit(line, 
+                 "detail?r=", fixed = TRUE)[[1]][2], "\">")[[1]][1])
+    cat("current revision number:", current_revision, "\n")
+    if (revision == "newest") {
       webdir <- "http://r4ss.googlecode.com/svn/trunk/"
-    }else{
-      if(is.numeric(revision) && revision<=current_revision){
-        webdir <- paste("http://r4ss.googlecode.com/svn-history/r",revision,"/trunk/",sep="")
-      }else{
-        stop("'revision' input should either be 'newest', or an integer <",current_revision)
+    }
+    else {
+      if (is.numeric(revision) && revision <= current_revision) {
+        webdir <- paste("http://r4ss.googlecode.com/svn-history/r", 
+                        revision, "/trunk/", sep = "")
+      }
+      else {
+        stop("'revision' input should either be 'newest', or an integer <", 
+             current_revision)
       }
     }
-
-    cat("getting file names from",webdir,"\n")
-    lines <- readLines(webdir,warn=F)
-    filenames <- lines[grep('"*.R"',lines)]
-    for(i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i],"\">")[[1]][2]   # split along: ">
-    for(i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i],"</a>")[[1]][1] # split along: </a>
-    return(list(filenames=filenames, webdir=webdir))
+    cat("getting file names from", webdir, "\n")
+    lines <- readLines(webdir, warn = F)
+    filenames <- lines[grep("\"*.R\"", lines)]
+    for (i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i], 
+                                                            "\">")[[1]][2]
+    for (i in 1:length(filenames)) filenames[i] <- strsplit(filenames[i], 
+                                                            "</a>")[[1]][1]
+    return(list(filenames = filenames, webdir = webdir))
   }
-
-  getwebfiles <- function(fileinfo){
+  getwebfiles <- function(fileinfo) {
     filenames <- fileinfo$filenames
     webdir <- fileinfo$webdir
-    # getting the files
     n <- length(filenames)
-    cat(n,"files found\n")
-    if(save) cat("saving all files to",local,"\n")
-    for(i in 1:n){
-      webfile <- paste(webdir,filenames[i],sep="/")
-      # hard wiring the updator file to be the latest version
-      if(filenames[i]=="update_r4ss_files.R")
+    cat(n, "files found\n")
+    if (save) 
+      cat("saving all files to", local, "\n")
+    for (i in 1:n) {
+      webfile <- paste(webdir, filenames[i], sep = "/")
+      if (filenames[i] == "update_r4ss_files.R") 
         webfile <- "http://r4ss.googlecode.com/svn/trunk/update_r4ss_files.R"
-      if(save){
-        localfile <- paste(local,filenames[i],sep="/")
+      if (save) {
+        localfile <- paste(local, filenames[i], sep = "/")
         temp <- readLines(webfile)
-        writeLines(temp,localfile)
-        cat("  writing ",localfile,"\n",sep="")
-      }else{
-        cat("  sourcing ",filenames[i],"\n",sep="")
-        source(webfile)
+        writeLines(temp, localfile)
+        cat("  writing ", localfile, "\n", sep = "")
+      }
+      else {
+        cat("  sourcing ", filenames[i], "\n", sep = "")
+        source(webfile,local=env)
       }
       flush.console()
     }
   }
-
-  getlocalfiles <- function(local){
-    filenames <- dir(local,pattern="*.R$")
+  getlocalfiles <- function(local) {
+    filenames <- dir(local, pattern = "*.R$")
     n <- length(filenames)
-    cat(n,"files found in",local,"\n")
-    for(i in 1:n){
-      cat("  sourcing ",filenames[i],"\n",sep="")
-      source(paste(local,filenames[i],sep="/"))
+    cat(n, "files found in", local, "\n")
+    for (i in 1:n) {
+      cat("  sourcing ", filenames[i], "\n", sep = "")
+      source(paste(local, filenames[i], sep = "/"),local=env)
       flush.console()
     }
   }
-
-  if(is.null(local)){
+  if (is.null(local)) {
     fileinfo <- getwebnames()
     getwebfiles(fileinfo)
-  }else{
-    if(save){
+  }
+  else {
+    if (save) {
       fileinfo <- getwebnames()
       getwebfiles(fileinfo)
     }
@@ -74,4 +89,3 @@ update_r4ss_files <- function(local=NULL,save=FALSE,revision="newest"){
   }
   cat("update complete.\n")
 }
-
