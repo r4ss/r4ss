a604021e9a917f9a54bb2102d7d9e47f757ade2f
Ian Taylor <ian.taylor@noaa.gov>
2014-07-15 10:33:14 -0700

    attempted to improve path settings in SS_html
    Thanks to Simon Hoyle for pointing out some issues. This may not fix
    them.

--- R/SS_html.R
+++ R/SS_html.R
@@ -45,7 +45,7 @@ SS_html <- function(replist=NULL,
   if(is.null(plotInfoTable)){
     if(!is.null(replist)){
       dir <- replist$inputs$dir
-      filenames <- dir(paste(dir,plotdir,sep="/"))
+      filenames <- dir(file.path(dir,plotdir))
       # look for all files beginning with the name 'plotInfoTable'
       filenames <- filenames[grep("plotInfoTable",filenames)]
       filenames <- filenames[grep(".csv",filenames)]
@@ -53,7 +53,7 @@ SS_html <- function(replist=NULL,
       plotInfoTable <- NULL
       # loop over matching CSV files and combine them
       for(ifile in 1:length(filenames)){
-        filename <- paste(dir,plotdir,filenames[ifile],sep="/")
+        filename <- file.path(dir,plotdir,filenames[ifile])
         temp <- read.csv(filename,colClasses = "character")
         plotInfoTable <- rbind(plotInfoTable,temp)
       }
@@ -100,7 +100,7 @@ SS_html <- function(replist=NULL,
   plotInfoTable$basename <- basename(as.character(plotInfoTable$file))
   plotInfoTable$dirname <- dirname(as.character(plotInfoTable$file))
   plotInfoTable$dirname2 <- basename(dirname(as.character(plotInfoTable$file)))
-  plotInfoTable$path <- paste(plotInfoTable$dirname2,plotInfoTable$basename,sep="/")
+  plotInfoTable$path <- file.path(plotInfoTable$dirname2,plotInfoTable$basename)
   dir <- dirname(plotInfoTable$dirname)[1]
 
   # write unique HTML file for each category of plots (or whatever)
@@ -108,12 +108,12 @@ SS_html <- function(replist=NULL,
   for(icat in 0:length(categories)){
     if(icat==0){
       category <- "Home"
-      htmlfile <- paste(dir,plotdir,"SS_output.html",sep="/")
+      htmlfile <- file.path(dir,plotdir,"SS_output.html")
       htmlhome <- htmlfile
       if(verbose) cat("Home HTML file with output will be:\n",htmlhome,'\n')
     }else{
       category <- categories[icat]
-      htmlfile <- paste(dir,"/",plotdir,"/SS_output_",category,".html",sep="")
+      htmlfile <- file.path(dir,plotdir,paste("SS_output_",category,".html",sep=""))
     }
     # write HTML head including some CSS stuff about fonts and whatnot
     # source for text below is http://unraveled.com/publications/css_tabs/
