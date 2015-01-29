e23d32cd1789bfe349ed5267810d617f1a724456
allanhicks <fishmanHicks@gmail.com>
2015-01-28 16:54:37 -0800

    Added argument called 'new' to mcmc.out
    the 'new' argument controls whether or not a window is opened so that
    one can set up the plot environment before plotting.

--- R/mcmc.out.R
+++ R/mcmc.out.R
@@ -1,9 +1,9 @@
 #' Summarize, analyze and plot key MCMC output.
-#' 
+#'
 #' Makes four panel plot showing trace plots, moving average, autocorrelations,
 #' and densities for chosen parameters from MCMC output.
-#' 
-#' 
+#'
+#'
 #' @param directory Directory where all results are located, one level above
 #' directory for particular run.
 #' @param run Directory with files from a particular run.
@@ -29,6 +29,7 @@
 #' @param header Data file with header?
 #' @param sep Separator for data file passed to the \code{read.table} function.
 #' @param print Send to screen unless asked to print.
+#' @param new Logical whether or not to open a new plot window before plotting
 #' @author Ian Stewart
 #' @export
 #' @seealso \code{\link{mcmc.nuisance}}, \code{\link{SSgetMCMC}}
@@ -52,16 +53,17 @@ mcmc.out <- function (
           plots=TRUE, 				# show plots or not
           header=TRUE,				# data file with header?
           sep=",",				# sep for data file
-          print=FALSE 				# send to screen unless asked to print
+          print=FALSE, 				# send to screen unless asked to print
+          new=T                 #open a new window
          )
- 
+
   # sample call: mcmc.out(run="english_8.4\\",names=T,burn=10,thin=1,scatter=F,stats=T,plots=T)
 
 ##############################################################################################################
-	# Purpose: To summarize,analyze and plot key MCMC output 
+	# Purpose: To summarize,analyze and plot key MCMC output
 	# Written: Ian Stewart, July 2003
      	# Arguments: See above
-     	# Returns: Graphical devices containing plots and summaries, 
+     	# Returns: Graphical devices containing plots and summaries,
      	#          parameter plots are stacked in the history, page up and down scrolls through them
      	# Notes: columns with fixed values will cause the diagnostic tests to crash
 ##############################################################################################################
@@ -71,7 +73,7 @@ mcmc.out <- function (
   ## geterrmessage()
   ## require(gtools) || stop("package gtools is required")
   ## geterrmessage()
-     
+
   # add section to set up for printing or display to screen (default)
   if(print==TRUE){}# not implemented
 
@@ -81,7 +83,7 @@ mcmc.out <- function (
     ###       by Brian Ripley as "against CRAN policies".
     #rm(.SavedPlots,pos=1) 					# remove any plotting history
    }
-  	
+
   filename  <- paste(directory,run,file,sep="")			# put directory,run and file names together for use
   if(!file.exists(filename))
     stop("file doesn't exist, try again jackass:\n",filename)   # warning if file does not exist
@@ -97,18 +99,18 @@ mcmc.out <- function (
    {
     nameout  <- paste(directory,run,namefile,sep="")		# put directory,run and file names together for use
     namedata <- read.table(nameout, 				# make data table of whole file
-                           header = FALSE, 			# no headers 
+                           header = FALSE, 			# no headers
                            sep = "", 				# space delimited
                            colClasses="character", 		# don't convert to factors
                            fill = TRUE)				# fill empty cells to make a symmetrical array
-  
+
     numparams <- as.numeric(namedata[1,1]) 			# get the dimension of the output
 
     # add names to the data table, only used in the scatterplot of all parameters
     for (j in 1:numparams) 					# loop over the moveparam columns
      {
       names(mcmcdata)[j] <- namedata[(j+1),1]			# name each column
-     } 
+     }
    }
 
   ##### change to mcmc object for coda #####
@@ -121,8 +123,8 @@ mcmc.out <- function (
 
   ##### plotting section #####
   if(plots==TRUE) 					# have plots been activated by user
-   {  
-    dev.new(record=TRUE) 				# keep the window open for each parameter
+   {
+    if(new) dev.new(record=TRUE) 				# keep the window open for each parameter
   if(numparams==5||numparams==9||numparams==13||numparams==17) 	# plots a blank plot if 5,9,13, or 17 plots created
    {							# this avoids the loss of plot n-1 in history (is this an R bug??)
     plot(0,0,
@@ -130,14 +132,14 @@ mcmc.out <- function (
          ylab="",
          frame.plot=FALSE,
          yaxt="n", 					# turn off this axis
-         xaxt="n",           
-         type="n") 					# plot nothing     
+         xaxt="n",
+         type="n") 					# plot nothing
    }
 
     for(i in 1:numparams) 				# loop over the number of parameters
      {
       par(new=FALSE,					# make sure to use a new graphical window
-          mfrow=c(2,2), 				# set up "cells" to graph into	
+          mfrow=c(2,2), 				# set up "cells" to graph into
           ann=TRUE) 					# annotate plots
 
       ##### Trace plot section #####
@@ -165,7 +167,7 @@ mcmc.out <- function (
                cex=1) 					# scale the text size
        }
 
-      #### Cumulative plot section #### 
+      #### Cumulative plot section ####
       lowest <- min(mcmcobject[,i]) 			# minimum value in chain
       highest <- max(mcmcobject[,i]) 			# maximum value in chain, for graphing
       plot(c(seq(1,draws,by=1)), 			# the x values
@@ -173,7 +175,7 @@ mcmc.out <- function (
            xlab="Iterations",
            ylab="",
            yaxt="n", 					# turn off this axis
-           type="n") 					# plot nothing 
+           type="n") 					# plot nothing
 
       if(!exists("running")){ # temporary turning off if function "running" is not present
         cat("skipping running average section because function 'running' is needed\n")
@@ -181,8 +183,8 @@ mcmc.out <- function (
         lines(running(mcmcobject[,i],
                       fun=median, 				# plot the mean
                       allow.fewer=TRUE,  			# begin calculating from the first point
-                      width=draws))			# Averages the last - observations (all in this case)  
-        
+                      width=draws))			# Averages the last - observations (all in this case)
+
         fun <- function(x,prob) quantile(x,probs=prob,names=FALSE) 	# the quantile to use in next 2 lines
 
         lines(running(mcmcobject[,i],
@@ -190,13 +192,13 @@ mcmc.out <- function (
                       prob=0.05,
                       allow.fewer=TRUE,  			# begin calculating from the first point
                       width=draws),
-              col="GREY")			
+              col="GREY")
         lines(running(mcmcobject[,i],
                       fun=fun, 				# function to use
                       prob=0.95,
                       allow.fewer=TRUE,  			# begin calculating from the first point
                       width=draws),
-              col="GREY")				
+              col="GREY")
       } # end temporary turning off
       #### Autocorrelation plot section ####
       par(ann=FALSE) 					# turn off default annotation
@@ -233,11 +235,11 @@ mcmc.out <- function (
      } 						# end parameter loop
    } 					# end plotting loop
 
-   #### Statistics section #####				
-    if(stats == TRUE) 				
+   #### Statistics section #####
+    if(stats == TRUE)
      {
       dev.new()						# opens a new graphics device
-      par(mar=c(0,0,3,0)) 				# makes the margins large 
+      par(mar=c(0,0,3,0)) 				# makes the margins large
       plot(0,  						# plot a graph of a single point
            ylab="",					# label the y-axis for whole screen
            xlab="", 					# label the x-axis for whole screen
@@ -308,10 +310,10 @@ mcmc.out <- function (
 
        l1.ac <- acf(mcmcobject[,i],lag.max=1,type="correlation",plot=F)
        acoruse <- round(l1.ac$acf[2],6)
-       text(13,25-i,acoruse,font=1,cex=0.9,adj=0)       
+       text(13,25-i,acoruse,font=1,cex=0.9,adj=0)
 
        effsize<- effectiveSize(mcmcobject[,i])
-       text(16.5,25-i,round(min(effsize,draws),0),font=1,cex=0.9,adj=0)       
+       text(16.5,25-i,round(min(effsize,draws),0),font=1,cex=0.9,adj=0)
 
        if(acoruse > 0.4)
         {gewuse <- "None"}
@@ -320,8 +322,8 @@ mcmc.out <- function (
          geweke <- geweke.diag(mcmcobject[,i],frac1=0.1,frac2=0.5)
          gewuse <- round(geweke$z,3)
         }
-       text(19,25-i,gewuse,font=1,cex=0.9,adj=0) 
-       
+       text(19,25-i,gewuse,font=1,cex=0.9,adj=0)
+
        if(acoruse > 0.4)
         {send <- "None"}
        if(acoruse <= 0.4)
@@ -331,11 +333,11 @@ mcmc.out <- function (
          if(hw[1]==1){send <- "Passed"}
         }
        text(22.5,25-i,send,font=1,cex=0.9,adj=0)
-       } 						# close the parameter loop 
+       } 						# close the parameter loop
      }			 				# end stats statement
 
    ##### Scatter plot section #####
-    if(scatter == TRUE) 				
+    if(scatter == TRUE)
      {
       dev.new()
       par(xaxt="n",yaxt="n") 				# suppress the axis labels
@@ -351,8 +353,8 @@ mcmc.out <- function (
       par(new=FALSE) 					# use a new window
       hist2d(mcmcobject[,surf1],			# x data as a vector
              mcmcobject[,surf2], 			# y data as a vector
-             nbins=100,  
-             na.rm=TRUE, 
+             nbins=100,
+             na.rm=TRUE,
              xlab=paste("parameter",surf1),
              ylab=paste("parameter",surf2),
              show=TRUE, 				# make figure or not
