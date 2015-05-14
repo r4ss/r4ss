195f8c90530d474cd5535d455999c2ce27ad3a33
Ian Taylor <ian.taylor@noaa.gov>
2015-05-14 08:15:00 -0700

    worked on SS_varadjust
    Getting closer to working properly, still unfinished

--- R/SS_varadjust.R
+++ R/SS_varadjust.R
@@ -1,43 +1,73 @@
 #' Modify variance and sample size adjustments in the control file
-#' 
-#' Incomplete function in process of development.
-#' 
+#'
+#' Function has not been fully tested yet
+#'
 #' @param dir Directory with control file to change.
 #' @param ctlfile Control file name. Default="control.ss_new".
 #' @param newctlfile Name of new control file to be written.
 #' Default="control_modified.ss".
 #' @param keyword Keyword to use as reference for start of section on
 #' variance adjustments
+#' @param newtable Optional table of new variance adjustment values
+#' @param newrow Optional vector of new variance adjustment values for a particular row
+#' @param rownumber Which of the 6 rows to replace with 'newrow' if present?
 #' @param maxcols Maximum number of columns to search among (may need to
 #' increase from default if you have a huge number of fleets)
+#' @param overwrite Overwrite file if it exists?
 #' @param verbose TRUE/FALSE switch for amount of detail produced by function.
 #' Default=TRUE.
 #' @author Ian Taylor
 #' @seealso \code{\link{SS_parlines}}, \code{\link{SS_changepars}}
 #' @export
-#' 
+#'
 SS_varadjust <- function(dir="C:/myfiles/mymodels/myrun/",
                          ctlfile="control.ss_new",
                          newctlfile="control_modified.ss",
                          keyword="Variance_adjustments",
-                         maxcols=50, 
-                         verbose=TRUE)
-{
-  # read control file as a table of values
-  if(!is.null(dir)) ctlfile <- file.path(dir,'control.ss_new')
+                         newtable=NULL, newrow=NULL, rownumber=NULL,
+                         maxcols=100, overwrite=FALSE,
+                         verbose=TRUE){
+  # check for consistency of inputs
+  if(!is.null(newtable)){
+    if(!is.null(newrow)){
+      stop("You can't input both 'newtable' and 'newrow'")
+    }
+    if(!is.data.frame(newtable) || nrow(newtable)!=6){
+      stop("Input 'newtable' must be a data.frame with 6 rows")
+    }
+  }
+  if(!is.null(newrow) & is.null(rownumber)){
+    stop("Input 'newrow' requires the input 'rownumber' (an integer from 1 to 6)")
+  }
+  if(!is.null(rownumber) && !rownumber %in% 1:6){
+    stop("Input 'rownumber' should be an integer specifying which of the 6 rows\n",
+         "of the variance adjustment table will be replaced with 'newrow'")
+  }
+  # combine directory and filenames
+  if(!is.null(dir)){
+    ctlfile <- file.path(dir, ctlfile)
+    newctlfile <- file.path(dir, newctlfile)
+  }
+  # read control file as a set of strings
   ctl_lines <- readLines(ctlfile)
+  # find line matching keyword and complain if 0 or 2+ lines found
   keyword_line <- grep(keyword, ctl_lines)
   if(length(keyword_line)!=1){
     stop("keyword input",keyword,"found",length(keyword_line),"times.\n",
          "It should be a unique string immediately before variance adjustments.")
   }
+  # read control file as a table of values
   ctl <- read.table(file=ctlfile,col.names=1:maxcols,skip=keyword_line,
-                    fill=TRUE,
+                    nrows=10, fill=TRUE,
                     quote="",colClasses="character",comment.char="",
                     blank.lines.skip=FALSE)
-  numeric_rows <- which(!is.na(as.numeric(ctl[,1])))
+  # save warnings settings and then turn off "NAs introduced" warning
+  old_warn <- options()$warn
+  options(warn=-1)
   # subset first 6 numeric rows
-  ctl <- ctl[numeric_rows[1:6],]
+  numeric_rows <- which(!is.na(as.numeric(ctl[,1])))
+  good_rows <- numeric_rows[1:6]
+  ctl <- ctl[good_rows,]
   # loop over columns, converting to numeric, checking for non-numeric values
   nfleets <- NULL
   for(icol in 1:maxcols){
@@ -47,6 +77,8 @@ SS_varadjust <- function(dir="C:/myfiles/mymodels/myrun/",
       nfleets <- icol-1
     }
   }
+  #returning to old warning value
+  options(warn=old_warn)
   # subset numeric columns only
   ctl <- ctl[,1:nfleets]
   # add header
@@ -58,50 +90,57 @@ SS_varadjust <- function(dir="C:/myfiles/mymodels/myrun/",
                              "#_mult_by_lencomp_N",
                              "#_mult_by_agecomp_N",
                              "#_mult_by_size-at-age_N"))
-  return(ctl)
+  cat("Existing table of variance adjustments:\n")
+  print(ctl)
   
-  ## ctl_num <- matrix(NA,nrows,ncols) # copy of ctl converted to numerical values or NA
-  ## num_cnt <- rep(NA,nrows)          # count of number of numerical values in each row
-  ## num_cnt7 <- rep(NA,nrows)         # count of number of numerical values in first 7 values of each row
-  ## num_cnt14 <- rep(NA,nrows)        # count of number of numerical values in first 14 values of each row
-  ## options(warn = -1)                # temporarily turn off "Warning: NAs introduced by coercion"
-  ## for(irow in 1:nrows){
-  ##   ctl_num[irow,] <- as.numeric(ctl[irow,])
-  ##   num_cnt[irow] <- sum(!is.na(ctl_num[irow,]))
-  ##   num_cnt7[irow] <- sum(!is.na(ctl_num[irow,1:7]))
-  ##   num_cnt14[irow] <- sum(!is.na(ctl_num[irow,1:14]))
-  ## }
-  ## options(warn = 1)                 # turn warnings back on
-  ## parlines7  <- ctl[num_cnt7==7 & is.na(ctl_num[,8]), ]
-  ## parlines14 <- ctl[num_cnt14==14 & is.na(ctl_num[,15]), ]
+  # absolute position of the rows to change
+  good_rows_absolute <- keyword_line + good_rows
 
-  ## parlines7   <- parlines7[, 1:9]
-  ## parlines14  <- parlines14[,1:16]
-  
-  ## namesvec7  <- c("LO", "HI", "INIT", "PRIOR", "PR_type", "SD", "PHASE", "Label", "Label2")
-  ## namesvec14 <- c("LO", "HI", "INIT", "PRIOR", "PR_type", "SD", "PHASE",
-  ##                 "env-var", "use_dev", "dev_minyr", "dev_maxyr", "dev_stddev", "Block", "Block_Fxn",
-  ##                 "Label", "Label2")
+  # this command will hopefully prevent getting stuck with all
+  # output written to the file after the function crashes before closing connection
+  on.exit({if(sink.number()>0) sink()})
 
-  ## names(parlines7 ) <- namesvec7
-  ## names(parlines14) <- namesvec14
+  # check for existence of file and warn if present and overwrite=FALSE
+  if(file.exists(newctlfile)){
+    if(!overwrite){
+      stop("File exists and input 'overwrite'=FALSE:\n      ",newctlfile,"\n")
+    }else{
+      file.remove(newctlfile)
+    }
+  }
 
-  ## if(print14){
-  ##   parlines7 <- cbind(parlines7[,1:7],matrix(NA,nrow=1,ncol=7),parlines7[,8:9])
-  ##   names(parlines7) <- namesvec14
-  ##   parlines <- rbind(parlines7,parlines14)
-  ## }else{
-  ##   parlines <- rbind(parlines7,parlines14[,c(1:7,15:16)])
-  ## }
-  
-  ## parlines$Label[parlines$Label=="#"] <- parlines$Label2[parlines$Label=="#"]
-  ## parlines <- parlines[,names(parlines)!="Label2"] # dropping the Label2 column
-  
-  ## parlines$Linenum <- as.numeric(rownames(parlines))
-  ## parlines <- parlines[order(parlines$Linenum),]
-  ## for(i in 1:7) parlines[,i] <- as.numeric(parlines[,i])
+  # open connection to file
+  if(verbose) cat("opening connection to",newctlfile,"\n")
+  zz <- file(newctlfile, open="at")
+  sink(zz)
+  # change maximum number of columns
+  oldwidth <- options()$width
+  oldmax.print <- options()$max.print
+  options(width=5000,max.print=9999999)
 
-  ## if(active) parlines <- parlines[parlines$PHASE > 0,]
-  ## return(parlines)
+  printdf <- function(dataframe){
+    # function to print data frame with hash mark before first column name
+    names(dataframe)[1] <- paste("#_",names(dataframe)[1],sep="")
+    print(dataframe, row.names=FALSE, strip.white=TRUE)
+  }
+
+  ### write file
+  # stuff prior to variance adjustments
+  writeLines(ctl_lines[1:(min(good_rows_absolute)+1)])
+  # table of variance adjustments
+  printdf(ctl)
+  # stuff after variance adjustments
+  writeLines(ctl_lines[(max(good_rows_absolute)+1):length(ctl_lines)])
+
+  # return maximum number of columns
+  options(width=oldwidth,max.print=oldmax.print)
+  # close connection
+  sink()
+  close(zz)
+  if(verbose){
+    cat("file written to", newctlfile, "\n")
+  }
+  # return table of values
+  return(invisible(ctl))
 } # end function
 
