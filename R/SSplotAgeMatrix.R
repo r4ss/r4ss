a0fd779aa7bba7523eeef08e506a992c1fc91d72
Ian Taylor <ian.taylor@noaa.gov>
2016-07-15 11:01:30 -0700

    more progress on SSplotAgeMatrix

--- R/SSplotAgeMatrix.R
+++ R/SSplotAgeMatrix.R
@@ -16,7 +16,7 @@ SSplotAgeMatrix <- function(replist, option=1, scale=3,
                                 "Length",            #2
                                 "True age",          #3
                                 "Observed age",      #4
-                                "ageing error type", #5
+                                "for ageing error type", #5
                                 "Distribution of",   #6
                                 "at"),               #7
                             cex.main=1, addmain=TRUE){
@@ -28,25 +28,21 @@ SSplotAgeMatrix <- function(replist, option=1, scale=3,
 
   if(option==1){
     # option 1 is plotting distribution of length at age
-    mat <- replist$ALK
-    # temporarily choosing the first morph/sex/etc of the array for simplicity
-    mat <- mat[,,1]
+    array <- replist$ALK
     # vertical dimension is plotting length bins
     ybins <- replist$lbinspop
     xlab <- "Age"
     ylab <- "Length"
-    main <- paste(labels[5], labels[2], labels[6], labels[1])
+    titlestart <- paste(labels[6], tolower(paste(labels[2], labels[7], labels[1])))
   }
   if(option==2){
     # option 2 is plotting distribution of observed age at true age
-    mat <- replist$AAK
-    # temporarily choosing the first ageing error matrix for simplicity
-    mat <- mat[1,,]
+    array <- replist$AAK
     # age bins
     ybins <- replist$agebins
     xlab <- "True age"
     ylab <- "Observed age"
-    main <- paste(labels[5], labels[4], labels[6], labels[3])
+    titlestart <- paste(labels[6], tolower(paste(labels[4], labels[7], labels[3])))
   }
   nybins <- length(ybins)
   
@@ -66,41 +62,62 @@ SSplotAgeMatrix <- function(replist, option=1, scale=3,
   ##                            ceiling(nages/2), byrow=TRUE))[1:nages]
 
   ymax <- 1.1*(ybins[nybins] + ybins[nybins] - ybins[nybins-1])
-  plot(0, type='n', las=1,
-       xlim=c(0,1.1*accuage), xaxs='i',
-       ylim=c(0, ymax), yaxs='i',
-       xlab=xlab, ylab=ylab, main=main)
-  abline(h=ybins, v=0:accuage, col='grey90')
-  for(iage in nages:1){
-    #print(iage)
-    #print(dim(mat))
-    a <- agevec[iage] # actual age
-    yvec <- rev(mat[,iage])
-    for(iybin in 1:nybins){
-      # lower limit of bin is value in vector of bins
-      ybin_lo <- ybins[iybin]
-      # upper limit is following bin...
-      if(iybin < length(ybins)){
-        ybin_hi <- ybins[iybin+1]
-      }else{
-        # unless it's the final bin in which case
-        # it's depicted as equal in width to the previous bin even
-        # though it's actually a plus group
-        ybin_hi <- ybins[iybin] +
-          (ybins[iybin] - ybins[iybin-1])
-      }
-      rect(xleft = a, 
-           ybottom = ybin_lo,
-           xright = a + scale*yvec[iybin],
-           ytop = ybin_hi,
-           col=colvec[iage])
-    }
-    #lines(a+yvec*scale, ybins)
-  }
-  
-  box()
-}
 
+  AgeMatrix.fn <- function(slice=1){
+    if(option==1){
+      # choose which morph/sex/etc of the array 
+      mat <- array[,,slice]
+      # need to figure out which slice corresponds to which
+      # morph/sex/etc. for labeling purposes
+      main <- titlestart 
+    }
+    if(option==2){
+      # choose which ageing error matrix
+      mat <- array[slice,,]
+      # concatenate title
+      main <- paste(titlestart, "\n", labels[5], slice)
+    }
+
+    plot(0, type='n', las=1,
+         xlim=c(0,1.1*accuage), xaxs='i',
+         ylim=c(0, ymax), yaxs='i',
+         xlab=xlab, ylab=ylab, main=main)
+    abline(h=ybins, v=0:accuage, col='grey90')
+    for(iage in nages:1){
+      #print(iage)
+      #print(dim(mat))
+      a <- agevec[iage] # actual age
+      yvec <- rev(mat[,iage]) # vector of values
+      for(iybin in 1:nybins){
+        # lower limit of bin is value in vector of bins
+        ybin_lo <- ybins[iybin]
+        # upper limit is following bin value...
+        if(iybin < length(ybins)){
+          ybin_hi <- ybins[iybin+1]
+        }else{
+          # unless it's the final bin in which case
+          # it's depicted as equal in width to the previous bin even
+          # though it's actually a plus group
+          ybin_hi <- ybins[iybin] +
+            (ybins[iybin] - ybins[iybin-1])
+        }
+        # add a filled rectangle for this combination of age and ybin
+        rect(xleft = a, 
+             ybottom = ybin_lo,
+             xright = a + scale*yvec[iybin],
+             ytop = ybin_hi,
+             col=colvec[iage])
+      } # end loop over bins (length or observed age)
+      #lines(a+yvec*scale, ybins)
+    } # end loop over true ages
+    box()
+  } # end of internal AgeMatrix.fn
+
+  AgeMatrix.fn()
+  
+} # end of function
+
+## Ian's notes on testing:
 # directory obviously specific to my computer
 #s2 <- SS_output('C:/ss/SSv3.30beta_May17/simple_3.30_ALK_tol')
 #SSplotALK(s2)
