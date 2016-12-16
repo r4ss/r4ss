e13e349ae42c70ea34ce2566028241b9aa4929c8
Ian Taylor <ian.taylor@noaa.gov>
2016-12-15 21:42:18 -0800

    SS_readforecast closer to working on 3.30
    Only half-finished for version 3.30 forecast files but should continue
    to work for 3.24 files. New "version" input is required to read 3.30
    forecast files. also function now stops if Forecast=0 unless new
    "readAll" input is set to TRUE. This is progress toward issue #128.

--- R/SS_readforecast.R
+++ R/SS_readforecast.R
@@ -7,6 +7,10 @@
 #' @param Nfleets Number of fleets.
 #' @param Nareas Number of areas.
 #' @param nseas number of seasons
+#' @param version SS version number. Currently only "3.24" or "3.30" are supported,
+#' either as character or numeric values (noting that numeric 3.30  = 3.3).
+#' @param readAll Should the function continue even if Forecast=0
+#' (at which point SS stops reading)
 #' @param verbose Should there be verbose output while running the file?
 #' @author Ian Taylor
 #' @export
@@ -14,30 +18,36 @@
 #' \code{\link{SS_writestarter}},
 #' \code{\link{SS_writeforecast}}, \code{\link{SS_writedat}},
 
-SS_readforecast <-  function(file='forecast.ss', Nfleets, Nareas, nseas,verbose=TRUE){
+SS_readforecast <-  function(file='forecast.ss', Nfleets, Nareas, nseas,
+                             version="3.24", readAll=FALSE, verbose=TRUE){
   # function to read Stock Synthesis forecast files
-  if(verbose) cat("running SS_readsforecast\n")
+  if(!(version=="3.24" | version=="3.30" | version==3.3)){
+    # turns out 3.30 != "3.30" in R
+    stop('version must be either 3.24 or 3.30')
+  }
+
+  if(verbose) cat("running SS_readforecast\n")
   forecast <- readLines(file,warn=F)
   mylist <- list()
 
   mylist$sourcefile <- file
   mylist$type <- "Stock_Synthesis_forecast_file"
-  mylist$SSversion <- "SSv3.21_or_later"
+  mylist$SSversion <- version
 
 
   # get numbers (could be better integrated with function above)
   allnums <- NULL
   for(i in 1:length(forecast)){
-      # split apart numbers from text in file
-      mysplit <- strsplit(forecast[i],split="[[:blank:]]+")[[1]]
-      mysplit <- mysplit[mysplit!=""]
-      nums <- suppressWarnings(as.numeric(mysplit))
-      if(sum(is.na(nums)) > 0) maxcol <- min((1:length(nums))[is.na(nums)])-1
-      else maxcol <- length(nums)
-      if(maxcol > 0){
-          nums <- nums[1:maxcol]
-          allnums <- c(allnums, nums)
-      }
+    # split apart numbers from text in file
+    mysplit <- strsplit(forecast[i],split="[[:blank:]]+")[[1]]
+    mysplit <- mysplit[mysplit!=""]
+    nums <- suppressWarnings(as.numeric(mysplit))
+    if(sum(is.na(nums)) > 0) maxcol <- min((1:length(nums))[is.na(nums)])-1
+    else maxcol <- length(nums)
+    if(maxcol > 0){
+      nums <- nums[1:maxcol]
+      allnums <- c(allnums, nums)
+    }
   }
 
   # go through numerical values and save as elements of a big list
@@ -46,70 +56,106 @@ SS_readforecast <-  function(file='forecast.ss', Nfleets, Nareas, nseas,verbose=
   mylist$MSY <- allnums[i]; i <- i+1
   mylist$SPRtarget <- allnums[i]; i <- i+1
   mylist$Btarget <- allnums[i]; i <- i+1
-  mylist$Bmark_years <- allnums[i:(i+5)]; i <- i+6
+  if(version==3.24){
+    mylist$Bmark_years <- allnums[i:(i+5)]; i <- i+6
+  }else{
+    mylist$Bmark_years <- allnums[i:(i+5)]; i <- i+10
+  }
+  if(verbose){
+    cat("Benchmark years: ", mylist$Bmark_years, "\n")
+  }
   mylist$Bmark_relF_Basis <- allnums[i]; i <- i+1
-  mylist$Forecast <- allnums[i]; i <- i+1
-  mylist$Nforecastyrs <- allnums[i]; i <- i+1
-  mylist$F_scalar <- allnums[i]; i <- i+1
-  mylist$Fcast_years <- allnums[i:(i+3)]; i <- i+4
-  mylist$ControlRuleMethod <- allnums[i]; i <- i+1
-  mylist$BforconstantF <- allnums[i]; i <- i+1
-  mylist$BfornoF <- allnums[i]; i <- i+1
-  mylist$Flimitfraction <- allnums[i]; i <- i+1
-  mylist$N_forecast_loops <- allnums[i]; i <- i+1
-  mylist$First_forecast_loop_with_stochastic_recruitment <- allnums[i]; i <- i+1
-  mylist$Forecast_loop_control_3 <- allnums[i]; i <- i+1
-  mylist$Forecast_loop_control_4 <- allnums[i]; i <- i+1
-  mylist$Forecast_loop_control_5 <- allnums[i]; i <- i+1
-  mylist$FirstYear_for_caps_and_allocations <- allnums[i]; i <- i+1
-  mylist$stddev_of_log_catch_ratio <- allnums[i]; i <- i+1
-  mylist$Do_West_Coast_gfish_rebuilder_output <- allnums[i]; i <- i+1
-  mylist$Ydecl <- allnums[i]; i <- i+1
-  mylist$Yinit <- allnums[i]; i <- i+1
-  mylist$fleet_relative_F <- allnums[i]; i <- i+1
-  if(mylist$fleet_relative_F==2) stop("SS_readforecast doesn't yet support option 2 for 'fleet relative F'")
-  mylist$basis_for_fcast_catch_tuning <- allnums[i]; i <- i+1
-  mylist$max_totalcatch_by_fleet <- allnums[i:(i+Nfleets-1)]; i <- i+Nfleets
-  if(verbose) cat("  max_totalcatch_by_fleet =",mylist$max_totalcatch_by_fleet,"\n")
-  mylist$max_totalcatch_by_area <- allnums[i:(i+Nareas-1)]; i <- i+Nareas
-  if(verbose) cat("  max_totalcatch_by_area =",mylist$max_totalcatch_by_area,"\n")
-  mylist$fleet_assignment_to_allocation_group <- allnums[i:(i+Nfleets-1)]; i <- i+Nfleets
-  # allocation groups
-  if(verbose) cat("  fleet_assignment_to_allocation_group =",mylist$fleet_assignment_to_allocation_group,"\n")
-  if(any(mylist$fleet_assignment_to_allocation_group!=0)){
-    mylist$N_allocation_groups <- max(mylist$fleet_assignment_to_allocation_group)
-    allocation_among_groups <- allnums[i:(i+mylist$N_allocation_groups*nseas-1)]; i <- i+mylist$N_allocation_groups*nseas
-    mylist$allocation_among_groups<-
-      as.data.frame(t(array(data=allocation_among_groups,dim=c(mylist$N_allocation_groups,nseas))))
-    colnames(mylist$allocation_among_groups)<-paste0("Grp",1:mylist$N_allocation_groups) 
-  }else{
-    mylist$N_allocation_groups <- 0
-    mylist$allocation_among_groups <- NULL
-  }
-  mylist$Ncatch <- Ncatch <- allnums[i]; i <- i+1
-  mylist$InputBasis <- allnums[i]; i <- i+1
-  # forcast catch levels
-  if(Ncatch==0){
-    ForeCatch <- NULL
-  }else{
-    ForeCatch <- data.frame(matrix(
-                                   allnums[i:(i+Ncatch*4-1)],nrow=Ncatch,ncol=4,byrow=TRUE))
-    i <- i+Ncatch*4
-    names(ForeCatch) <- c("Year","Seas","Fleet","Catch_or_F")
+  mylist$Forecast <- Forecast <- allnums[i]; i <- i+1
+  # test for 0 value of Forecast and only continue if non-zero or readAll
+  if(Forecast==0 & !readAll){
     if(verbose){
-      cat("  Catch inputs (Ncatch =",Ncatch,"\n")
-      print(ForeCatch)
+      cat("Forecast=0 and input readAll=FALSE so skipping remainder of file\n")
     }
-  }
-
-  mylist$ForeCatch <- ForeCatch
-  # check final value
-  if(allnums[i]==999){
-    if(verbose) cat("read of forecast file complete (final value = 999)\n")
   }else{
-    cat("Error: final value is", allnums[i]," but should be 999\n")
-  }
-
+    if(verbose){
+      cat("Forecast =", Forecast, "\n")
+    }
+    mylist$Nforecastyrs <- allnums[i]; i <- i+1
+    mylist$F_scalar <- allnums[i]; i <- i+1
+    if(version==3.24){
+      mylist$Fcast_years <- allnums[i:(i+3)]; i <- i+4
+    }else{
+      mylist$Fcast_years <- allnums[i:(i+3)]; i <- i+6
+    }
+    if(verbose){
+      cat("Forecast years: ", mylist$Fcast_years, "\n")
+    }
+    mylist$ControlRuleMethod <- allnums[i]; i <- i+1
+    mylist$BforconstantF <- allnums[i]; i <- i+1
+    mylist$BfornoF <- allnums[i]; i <- i+1
+    mylist$Flimitfraction <- allnums[i]; i <- i+1
+    mylist$N_forecast_loops <- allnums[i]; i <- i+1
+    mylist$First_forecast_loop_with_stochastic_recruitment <- allnums[i]; i <- i+1
+    mylist$Forecast_loop_control_3 <- allnums[i]; i <- i+1
+    mylist$Forecast_loop_control_4 <- allnums[i]; i <- i+1
+    mylist$Forecast_loop_control_5 <- allnums[i]; i <- i+1
+    mylist$FirstYear_for_caps_and_allocations <- allnums[i]; i <- i+1
+    mylist$stddev_of_log_catch_ratio <- allnums[i]; i <- i+1
+    mylist$Do_West_Coast_gfish_rebuilder_output <- allnums[i]; i <- i+1
+    mylist$Ydecl <- allnums[i]; i <- i+1
+    mylist$Yinit <- allnums[i]; i <- i+1
+    mylist$fleet_relative_F <- allnums[i]; i <- i+1
+    if(mylist$fleet_relative_F==2){
+      stop("SS_readforecast doesn't yet support option 2 for 'fleet relative F'")
+    }
+    mylist$basis_for_fcast_catch_tuning <- allnums[i]; i <- i+1
+    if(version==3.24){
+      if(verbose){
+        cat('reading section on fleet- and area-specific inputs based on 3.24 format\n')
+      }
+      # the following section is somewhat different between 3.24 and 3.30
+      mylist$max_totalcatch_by_fleet <- allnums[i:(i+Nfleets-1)]; i <- i+Nfleets
+      if(verbose) cat("  max_totalcatch_by_fleet =",mylist$max_totalcatch_by_fleet,"\n")
+      mylist$max_totalcatch_by_area <- allnums[i:(i+Nareas-1)]; i <- i+Nareas
+      if(verbose) cat("  max_totalcatch_by_area =",mylist$max_totalcatch_by_area,"\n")
+      mylist$fleet_assignment_to_allocation_group <- allnums[i:(i+Nfleets-1)]; i <- i+Nfleets
+      # allocation groups
+      if(verbose) cat("  fleet_assignment_to_allocation_group =",mylist$fleet_assignment_to_allocation_group,"\n")
+      if(any(mylist$fleet_assignment_to_allocation_group!=0)){
+        mylist$N_allocation_groups <- max(mylist$fleet_assignment_to_allocation_group)
+        allocation_among_groups <- allnums[i:(i+mylist$N_allocation_groups*nseas-1)]; i <- i+mylist$N_allocation_groups*nseas
+        mylist$allocation_among_groups<-
+          as.data.frame(t(array(data=allocation_among_groups,dim=c(mylist$N_allocation_groups,nseas))))
+        colnames(mylist$allocation_among_groups)<-paste0("Grp",1:mylist$N_allocation_groups) 
+      }else{
+        mylist$N_allocation_groups <- 0
+        mylist$allocation_among_groups <- NULL
+      }
+      mylist$Ncatch <- Ncatch <- allnums[i]; i <- i+1
+      mylist$InputBasis <- allnums[i]; i <- i+1
+      # forcast catch levels
+      if(Ncatch==0){
+        ForeCatch <- NULL
+      }else{
+        ForeCatch <- data.frame(matrix(
+            allnums[i:(i+Ncatch*4-1)],nrow=Ncatch,ncol=4,byrow=TRUE))
+        i <- i+Ncatch*4
+        names(ForeCatch) <- c("Year","Seas","Fleet","Catch_or_F")
+        if(verbose){
+          cat("  Catch inputs (Ncatch = ",Ncatch,")\n", sep="")
+          print(ForeCatch)
+        }
+      }
+    }
+    if(version=="3.30" | version==3.3){
+      if(verbose){
+        cat('reading section on fleet- and area-specific inputs based on 3.30 format\n')
+      }
+      stop("sorry, this part of the function is unfinished for 3.30 models")
+    }
+    mylist$ForeCatch <- ForeCatch
+    # check final value
+    if(allnums[i]==999){
+      if(verbose) cat("read of forecast file complete (final value = 999)\n")
+    }else{
+      cat("Error: final value is", allnums[i]," but should be 999\n")
+    }
+  } # end read of additional values beyond "Forecast" input
   # all done
   return(mylist)
 }
