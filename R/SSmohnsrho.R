09ac4d486e294588b7fa9a3ccc7ecb57d0265098
Chantel Wetzel <chantel.wetzel@noaa.gov>
2018-03-05 16:09:34 -0800

    Updated Mohn's Rho based on Carey McGilliard's suggestions

--- R/SSmohnsrho.R
+++ R/SSmohnsrho.R
@@ -1,7 +1,12 @@
 ##' Calculate Mohn's Rho values for select quantities
-##' Function calculates a rho value for the ending year for each retrospective
-##' relative to the reference model and a "Wood's Hole Mohn's Rho" which is based
-##' on all years between the reference and the retrospective run.
+##' Function calculates: 
+##' (1) a rho value for the ending year for each retrospective relative to the reference model, 
+##' (2) a "Wood's Hole Mohn's Rho", and 
+##' (3) an "Alaska Fisheries Science Center and Hurtado-Ferro et al. (2015) Mohn's rho"
+##' (2) and (3) are based on all years between the reference and the retrospective run.
+##' Reference: Hurtado-Ferro et al. 2015. Looking in the rear-view mirror: bias and 
+##' retrospective patterns in integrated, age-structured stock assessment models. ICES J. Mar. Sci
+##' 'Volume 72, Issue 1, 1 January 2015, Pages 99–110, https://doi.org/10.1093/icesjms/fsu198
 ##'
 ## Grab all the results 
 ##' @param summaryoutput List created by \code{SSsummarize}. The expected order for the 
@@ -11,13 +16,13 @@
 ##' @param startyr Single year used to calculate the start of the Wood's Hole
 ##' Mohn's Rho value across all years.
 ##'
-##' @author Chantel R. Wetzel
+##' @author Chantel R. Wetzel and Carey McGilliard
 ##'
 ##' @export
 
 SSmohnsrho <- function(summaryoutput, endyrvec = NULL, startyr = NULL){
 
-	print ("The expected order of model in the summary output are the reference model followed by retro -1, retro -2, and so forth.")
+	print ("The expected order of models in the summary output are the reference model followed by retro -1, retro -2, and so forth.")
 
 	N = summaryoutput$n
 	if(length(endyrvec) == 0) { endyrvec = rev( (summaryoutput$endyrs[N]-N+1) :summaryoutput$endyrs[N]) }
@@ -27,6 +32,8 @@ SSmohnsrho <- function(summaryoutput, endyrvec = NULL, startyr = NULL){
 	
 	mohnSSB = mohnRec = mohnBratio = mohnF = numeric()
 	mohnSSB.all = mohnRec.all = mohnBratio.all = mohnF.all = numeric()
+	HmohnSSB = HmohnRec = HmohnBratio = HmohnF = numeric()
+    HmohnSSB.all = HmohnRec.all = HmohnBratio.all = HmohnF.all = numeric()
 
 	# Mohn's Rho Calculation for the terminal year for each of the retrospectives relative to the reference model
 	# Rho = sum over y [ (X_y,retro - X_y,ref) / X_y,ref ]
@@ -43,6 +50,23 @@ SSmohnsrho <- function(summaryoutput, endyrvec = NULL, startyr = NULL){
 
 	  ind = which(summaryoutput$Fvalue$Yr == endyrvec[i])
 	  mohnF[i] = (summaryoutput$Fvalue[ind, i + 1] - summaryoutput$Fvalue[ind, 1]) / summaryoutput$Fvalue[ind, 1] 
+	}
+
+	# Alaska Fisheries Science Center and Hurtado-Ferro et al. (2015) Mohn's rho
+	# https://www.afsc.noaa.gov/REFM/stocks/Plan_Team/2013/Sept/Retrospectives_2013_final3.pdf
+	# Equation 1:  Rho = (sum over p [ (X_y-p,p -X_y-p,0) / X_y-p,0]) / P
+	for(i in 1:(N-1))
+      Hind = which(summaryoutput$SpawnBio$Yr == endyrvec[i+1])
+      HmohnSSB[i] = (summaryoutput$SpawnBio[Hind, i + 1] - summaryoutput$SpawnBio[Hind, 1]) / summaryoutput$SpawnBio[Hind, 1]
+      
+      Hind = which(summaryoutput$recruits$Yr == endyrvec[i+1])   
+      HmohnRec[i] = (summaryoutput$recruits[Hind, i + 1] - summaryoutput$recruits[Hind, 1]) / summaryoutput$recruits[Hind, 1]
+      
+      Hind = which(summaryoutput$Fvalue$Yr == endyrvec[i+1])   
+      HmohnF[i] = (summaryoutput$Fvalue[Hind, i + 1] - summaryoutput$Fvalue[Hind, 1]) / summaryoutput$Fvalue[Hind, 1]
+      
+      Hind = which(summaryoutput$Bratio$Yr == endyrvec[i+1])   
+      HmohnBratio[i] = (summaryoutput$Bratio[Hind, i + 1] - summaryoutput$Bratio[Hind, 1]) / summaryoutput$Bratio[Hind, 1]
 	}
 
 	# Wood's Hole Mohn's Rho Calculation for all years for each of the retrospectives relative to the reference model
@@ -75,9 +99,14 @@ SSmohnsrho <- function(summaryoutput, endyrvec = NULL, startyr = NULL){
   mohn.out$Bratio = sum(mohnBratio)
   mohn.out$F = sum(mohnF)
 
-  mohn.out$SSB.all = sum(mohnSSB.all)
-  mohn.out$Rec.all = sum(mohnRec.all)
-  mohn.out$Bratio.all = sum(mohnBratio.all)
-  mohn.out$F.all = sum(mohnF.all)
+  mohn.out$WoodHole_SSB.all = sum(mohnSSB.all)
+  mohn.out$WoodHole_Rec.all = sum(mohnRec.all)
+  mohn.out$WoodHole_Bratio.all = sum(mohnBratio.all)
+  mohn.out$WoodHole_F.all = sum(mohnF.all)
+
+  mohn.out$AFSC_Hurtado_SSB = sum(HmohnSSB) / length(HmohnSSB)
+  mohn.out$AFSC_Hurtado_Rec = sum(HmohnRec) / length(HmohnRec)
+  mohn.out$AFSC_Hurtado_F = sum(HmohnF) / length(HmohnF)
+  mohn.out$AFSC_Hurtado_Bratio = sum(HmohnBratio) / length(HmohnBratio)
   return(mohn.out)
 }
