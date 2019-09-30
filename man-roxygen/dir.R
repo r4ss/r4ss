535762a6bf16355833cbe5acbae64f8deb3e3284
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2019-09-30 12:17:33 -0700

    develop: write weight-at-age file to disk

---
+++ man-roxygen/dir.R
@@ -0,0 +1,7 @@
+#' @param dir A file path to the directory of interest.
+#' Typically used with \code{file}, an additional input argument,
+#' to specify input and output file paths.
+#' The default value is \code{dir = NULL}, which leads to using
+#' the current working directory, and thus, full file paths should
+#' not be specified for other arguments as they will be appended
+#' to \code{dir}.
