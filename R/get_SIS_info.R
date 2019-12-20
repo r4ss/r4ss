34a61f6c070d9da80560a20201c605b7553041a5
Ian Taylor <ian.taylor@noaa.gov>
2019-12-20 13:53:13 -0800

    feat: refine get_SIS_info for multi-area models and other changes

--- R/get_SIS_info.R
+++ R/get_SIS_info.R
@@ -25,22 +25,31 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
   if(is.null(dir)){
     dir <- model$inputs$dir
   }
-  
+
   # construct filename
   if (is.null(endyr)) {
     endyr <- model$endyr
   }
   filename <- paste(species, endyr, "SIS_info.csv", sep="_")
-  
+
   message("writing SIS info to CSV file:\n",
           file.path(dir, filename ))
-  
+
   # years to report for catch-related quantities
   startyr <- model$startyr
   years <- startyr:(endyr + 1)
-  
+
   # get values from TIME_SERIES table
   ts <- model$timeseries
+
+  # aggregate across areas if needed
+  if(model$nareas > 1){
+    ts.tmp <- ts # copy of table
+    ts <- ts.tmp[ts.tmp$Area == 1,]
+    for(iarea in 2:model$nareas){
+      ts[,-(1:4)] <- ts[,-(1:4)] + ts.tmp[ts.tmp$Area == iarea,-(1:4)]
+    }
+  }
   tab <- data.frame(Year = ts$Yr,
                     Total_Bio = round(ts$Bio_all),
                     Spawning_Bio = round(ts$SpawnBio),
@@ -50,7 +59,7 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
   if(tab$Total_Bio[tab$Year == endyr + 1] == 0){
     tab$Total_Bio[tab$Year == endyr + 1] <- NA
   }
-  
+
   # calculate total catch (aggregated across fleets)
   catch_tab <- aggregate(model$catch$kill_bio, by = list(model$catch$Yr),
                          FUN = function(x){round(sum(x))})
@@ -58,7 +67,7 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
   # filter years and set forecast values to NA
   catch_tab <- catch_tab[catch_tab$Year %in% years,]
   catch_tab[catch_tab$Year == endyr+1, -1] <- NA
-  
+
   if (endyr <= model$endyr) {
     # add NA value for endyr + 1 if not present (this was the case in SS version 3.30.12)
     if(endyr+1 %in% catch_tab$Year){
@@ -72,7 +81,7 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
     forecatch$Catch[nrow(forecatch)] = NA
     catch_tab = rbind(catch_tab, forecatch)
   } # end if-else
-  
+
   # calculate proxy-F values (e.g. exploitation rate)
   F_tab <- data.frame(Year = years,
                       Exploit_rate = round(model$derived_quants[paste0("F_", years), "Value"], 2))
@@ -82,19 +91,18 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
   # filter years and set forecast values to NA
   F_tab <- F_tab[F_tab$Year %in% years,]
   F_tab[F_tab$Year == endyr+1, -1] <- NA
-  
+
   # SPR-related quantities
-  spr_tab <- model$sprseries[, c("Yr", "SPR_report", "SPR")]
-  names(spr_tab) <- c("Year", model$SPRratioLabel, "SPR")
-  # add 1-SPR
+  spr_tab <- model$sprseries[, c("Yr", "SPR")]
+  # add 1-SPR (may have already existed as SPR_report
   spr_tab$"1-SPR" <- 1 - spr_tab$SPR
-  # re-order columns
-  spr_tab <- spr_tab[,c(1,2,4,3)]
+  names(spr_tab)[1] <- "Year"
+  spr_tab <- spr_tab[,c("Year","1-SPR","SPR")]
   # filter years and set forecast values to NA
   spr_tab <- spr_tab[spr_tab$Year %in% years,]
   spr_tab[spr_tab$Year == endyr+1, -1] <- NA
   spr_tab[,-1] <- round(spr_tab[,-1], 3)
-  
+
   tab <- merge(merge(merge(ts_tab, catch_tab), F_tab), spr_tab)
   if(nrow(tab) != length(years) || any(tab$Year != years)){
     stop("problem with mismatch of years:\n",
@@ -111,9 +119,30 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
   tab <- cbind("", tab)
 
   # quantities of interest
-  SPRtarg_text <- paste0("SPR", 100*model$sprtarg, "%") # e.g. SPR50%
-  Btarg_text <- paste0("B", 100*model$btarg, "%") # e.g. B40%
-  MinBthresh_text <- paste0("B", 100*model$minbthresh, "%") # e.g. B25%
+  if(model$sprtarg == -999){
+    SPRtarg_text <- "SPR_XX%"
+    model$sprtarg <- NA
+    warning('No value for model$sprtarg')
+  }else{
+    SPRtarg_text <- paste0("SPR", 100*model$sprtarg, "%") # e.g. SPR50%
+  }
+
+  if(model$btarg == -999){
+    Btarg_text <- "BXX%"
+    model$btarg <- NA
+    warning('No value for model$btarg')
+  }else{
+    Btarg_text <- paste0("B", 100*model$btarg, "%") # e.g. B40%
+  }
+
+  if(model$minbthresh == -999){
+    MinBthresh_text <- "BXX%"
+    model$minbthresh <- NA
+    warning('No value for model$minbthresh')
+  }else{
+    MinBthresh_text <- paste0("B", 100*model$minbthresh, "%") # e.g. B25%
+  }
+  
   Bcurrent <- tab$Spawning_Bio[tab$Year == endyr + 1]
 
   # MSY-proxy labels were changed at some point
@@ -123,9 +152,21 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
   }else{
     Yield_at_SPR_target <- round(model$derived_quants["TotYield_SPRtgt", "Value"])
     Yield_at_B_target <- round(model$derived_quants["TotYield_Btgt", "Value"])
-  }    
+  }
 
-  info_tab <- c(paste0("SAIP:,", "https://spo.nmfs.noaa.gov/sites/default/files/TMSPO183.pdf"),  
+  # age for summary biomass
+  summary_age <- model$summary_age
+  if(is.null(summary_age)){
+    summary_age <- "X"
+  }
+
+  # SS version
+  SS_version <- strsplit(model$SS_version, split = ";")[[1]][1]
+  gsub("-safe", "", SS_version)
+  gsub("-opt", "", SS_version)
+  
+  # make big 2-column table of info about each model
+  info_tab <- c(paste0("SAIP:,", "https://spo.nmfs.noaa.gov/sites/default/files/TMSPO183.pdf"),
                 "",
                 paste0("Stock / Entity Name,", species),
                 paste0("Assessment Type,", "X"),
@@ -133,18 +174,18 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
                 paste0("Asmt Year and Month,", "XXXX.XX"),
                 paste0("Last Data Year,", endyr),
                 paste0("Asmt Model Category,", "6 - Statistical Catch-at-Age (SS, ASAP, AMAK, BAM, MultifancL< CASAL)"),
-                paste0("Asmt Model Category,", "SS"), 
-                paste0("Model Version,", model$SS_versionshort),
-                paste0("Lead Lab,", "NWFSC"), 
-                paste0("Point of Contact (assessment lead),", "XXXX"), 
-                paste0("Review Result,", "Accepted"), 
-                paste0("Catch Input Data,", "XXXX"), 
+                paste0("Asmt Model Category,", "SS"),
+                paste0("Model Version,", SS_version),
+                paste0("Lead Lab,", "NWFSC"),
+                paste0("Point of Contact (assessment lead),", "XXXX"),
+                paste0("Review Result,", "Accepted"),
+                paste0("Catch Input Data,", "XXXX"),
                 paste0("Abundance Input Data,", "XXXX"),
                 paste0("Biological Input Data,", "XXXX"),
-                paste0("Size/Age Composition Input Data,", "XXXX"), 
+                paste0("Size/Age Composition Input Data,", "XXXX"),
                 paste0("Ecosystem Linkage,", "XXXX"),
                 "",
-                
+
                 "Fishing Mortality Estimates",
                 paste0("F Year, ",          endyr),
                 paste0("Min F Estimate, ",  "not required and typically not entered"),
@@ -183,7 +224,7 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
                        round(model$derived_quants[paste0("Bratio_", endyr+1),
                                                   "Value"], 3)),
                 "",
-                       
+
                 "Domestic Biomass Derived Management Quantities",
                 paste0("Blimit, ",          model$SBzero*model$minbthresh),
                 paste0("Blimit Basis, ",    MinBthresh_text),
@@ -192,17 +233,21 @@ get_SIS_info <- function(model, dir = NULL, writecsv = TRUE,
                 paste0("B/Blimit, ",        Bcurrent/(model$SBzero*model$minbthresh)),
                 paste0("B/Bmsy, ",          Bcurrent/(model$SBzero*model$btarg)),
                 paste0("Stock Level (relative) to BMSY, ", "Above"),
-                paste0("age X+summary biomass, ",
+                paste0("age ", summary_age, "+ summary biomass, ",
                        model$timeseries$Bio_smry[model$timeseries$Yr == endyr]),
                 "",
                 "")
 
+  # write 2-column table at the top of the file
   write.table(info_tab, file = file.path(dir, filename),
               quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE)
-
-  write.table(tab, file = file.path(dir, filename),
-              quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE,
-              append = TRUE)
+  # add time series table with more columns (suppressing warning about
+  # 'appending column names to file')
+  suppressWarnings(
+      write.table(tab, file = file.path(dir, filename),
+                  quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE,
+                  append = TRUE)
+  )
 
   invisible(list(info_tab, tab))
 }
