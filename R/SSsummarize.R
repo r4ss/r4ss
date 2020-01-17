fdb0219b0849c7d2cf9f1e82d5a42b9f16b42571
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2020-01-17 07:57:55 -0800

    feature: allow for a different number of fleets using merge
    Use merge to combine likelihoods_by_fleet rather than matching
    column names. Merge allows all combinations to be kept.
    Could use this tactic on tagging data too.

--- R/SSsummarize.R
+++ R/SSsummarize.R
@@ -161,7 +161,7 @@ SSsummarize <- function(biglist,
          all(names(likelihoods_by_fleet)==names(liketemp2)))){
       likelihoods_by_fleet <- rbind(likelihoods_by_fleet,liketemp2)
     }else{
-      warning("problem summarizing likelihoods by fleet due to mismatched columns")
+      likelihoods_by_fleet <- merge(likelihoods_by_fleet, liketemp2, all = TRUE)
     }
 
     ## likelihoods by tag group
