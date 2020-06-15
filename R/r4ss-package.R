b9a6279cd1d8214615e0c1ceb955c88a1f615942
Ian Taylor <ian.taylor@noaa.gov>
2020-06-15 11:53:00 -0700

    docs: update note on SS versions tested against

--- R/r4ss-package.R
+++ R/r4ss-package.R
@@ -10,7 +10,7 @@
 #' 1.38.1\cr Date: \tab 2020-06-15\cr License: \tab GPL-3\cr LazyLoad: \tab
 #' yes\cr URL: \tab \url{https://github.com/r4ss/}\cr }
 #' Should be compatible with Stock Synthesis versions 3.24 through 3.30
-#' (specifically version 3.30.15.05, from May 2020).
+#' (specifically version 3.30.15.06, from June 2020).
 #'
 #' @name r4ss-package
 #' @aliases r4ss-package r4ss
