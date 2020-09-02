57d8fff4683273893b825726d76e207808878d01
Ian Taylor <ian.taylor@noaa.gov>
2020-09-02 15:59:47 -0700

    docs: increment to version 1.40.0, add LaTreese Denson to author list

--- R/r4ss-package.R
+++ R/r4ss-package.R
@@ -7,10 +7,10 @@
 #' parameterizations, and various other tasks.
 #'
 #' \tabular{ll}{ Package: \tab r4ss\cr Type: \tab Package\cr Version: \tab
-#' 1.39.1\cr Date: \tab 2020-08-13\cr License: \tab GPL-3\cr LazyLoad: \tab
+#' 1.40.0\cr Date: \tab 2020-09-02\cr License: \tab GPL-3\cr LazyLoad: \tab
 #' yes\cr URL: \tab \url{https://github.com/r4ss/}\cr }
 #' Should be compatible with Stock Synthesis versions 3.24 through 3.30
-#' (specifically version 3.30.15.09, from July 2020).
+#' (specifically version 3.30.16.00, from September 2020).
 #'
 #' @name r4ss-package
 #' @aliases r4ss-package r4ss
@@ -22,7 +22,7 @@
 #' Sean C. Anderson, Gwladys I. Lambert, Megan M. Stachura,
 #' Andrew B. Cooper, Andi Stephens, Neil L. Klaer, Carey R. McGilliard,
 #' Iago Mosqueira, Watal M. Iwasaki, Kathryn Doering, Andrea M. Havron,
-#' and Nathan Vaughan
+#' Nathan Vaughan, and LaTreese S. Denson
 #'
 #' Package maintainer: Ian G. Taylor <Ian.Taylor@@noaa.gov>
 #' @references r4ss on GitHub: \url{https://github.com/r4ss}
