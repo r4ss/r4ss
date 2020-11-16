043ceafb0cbba8cabba888b69ab2c7c0f6e48818
kathryn.doering <kathryn.doering@noaa.gov>
2020-11-16 15:35:08 -0800

    #430 , feat: increase flexibility of exe location for run_SS_models
    rm stop msg to allow any model path to be used. Note there are still some issues with using a model in your PATH vs not: even if mod_in_path = FALSE, this function may run a model in the PATH when trying to refer to a folder's model.
    
    Note this also fixes the model run status, which sometimes reported success when the run actuallly failed.

--- R/run_SS_models.R
+++ R/run_SS_models.R
@@ -3,7 +3,13 @@
 #' Loops over a vector of directories and iteratively runs SS in each one
 #'
 #' @param dirvec List of directories containing the model files
-#' @param model Executable name
+#' @param model Executable name or path to executable (absolute path, or relative
+#'  to the working directory). First, if exe_in_path is
+#'  FALSE, The function will look an executable with the same name in each
+#'  element of dirvec. Then, if it is not found in each, the function will
+#'  assume that model is the path to the executable and there is only 1 copy of
+#'  the executable. Note that if there is an exe in your PATH with the same 
+#'  name, this will be used even if exe_in_path is FALSE.
 #' @param extras Additional commands to use when running SS. Default = "-nox"
 #' will reduce the amount of command-line output.
 #' @param systemcmd Should R call SS using "system" function instead of "shell".
@@ -23,9 +29,11 @@
 #' @examples
 #'
 #'   \dontrun{
-#' dirvec <- dir("c:/SS/models_to_run", full.names=TRUE)
-#' run_SS_models(dirvec=dirvec)
-#' 
+#' extdata_mods <- system.file("extdata", package = "r4ss")
+#' dirvec <- c(file.path(extdata_mods, "simple_3.30.12"), 
+#'             file.path(extdata_mods, "simple_3.30.13"))
+#' # if ss or ss.exe is available in both directories:
+#' run_SS_models(dirvec = dirvec)
 #'   }
 #'
 
@@ -54,11 +62,10 @@ run_SS_models <- function(dirvec = NULL,
   if(length(grep(".exe",tolower(model))) == 1){
     # if input 'model' includes .exe then assume it's Windows and just use the name
     exe <- model
-  }else{
+  } else {
     # if 'model' doesn't include .exe then append it (for Windows computers only)
     exe <- paste(model, ifelse(OS=="windows",".exe",""),sep="")
   }
-  
   if(exe_in_path == TRUE) {
     tmp_exe <- Sys.which(exe)[[1]] # get 1st ss exe with name exe that is in your path
     if(tmp_exe == ""){
@@ -66,10 +73,41 @@ run_SS_models <- function(dirvec = NULL,
     } else {
       exe <- tmp_exe
     }
+  } else {
+    # check if model is in the same dir as each folder of dir vec. If not
+    # found in all folders, see if can find using the relative or absolute path.
+    # If can't find, revert back to assuming the model is in each folder of the
+    # dirvec.
+    all_exes_in_folder <- lapply(dirvec, function(dir, mod) {
+      exists <- file.info(file.path(dir, mod))$exe
+      if(is.na(exists) | exists == "no" ) {
+        exe_exists <- FALSE
+      } else {
+        exe_exists <- TRUE
+      }
+      exe_exists
+    }, mod = model)
+    if(!all(unlist(all_exes_in_folder) == TRUE)) {
+      exists_abs_path <- file.info(normalizePath(model))$exe
+      if(is.na(exists_abs_path) | (exists_abs_path) == "no" ) {
+        exe_exists_abs_path <- FALSE
+      } else {
+        exe_exists_abs_path <- TRUE
+      }
+      if(exe_exists_abs_path) {
+        message("Assuming path to the exe provided in model")
+        model <- (normalizePath(model))
+      } else {
+        message("Assuming model is in each dirvec folder, but missing from ", 
+                "some folders")
+      }
+    } else {
+      message("Assuming model is in each dirvec folder.")
+    }
   }
 
   # loop over directories
-  for(idir in 1:length(dirvec)){
+  for(idir in seq_along(dirvec)){
     # directory where stuff will happen
     dir <- dirvec[idir]
     
@@ -78,13 +116,6 @@ run_SS_models <- function(dirvec = NULL,
       warning("not a directory:", dir)
       results[idir] <- "not a directory"
     }else{
-      # check whether exe is in directory (if not using exe in path)
-      if(all(file.info(dir(dir, full.names=TRUE))$exe=="no")){
-        if(exe_in_path == FALSE){
-          warning("Executable ",exe," not found in ",dir)
-          results[idir] <- "no executable"
-        }
-      }
       if(skipfinished & "Report.sso" %in% dir(dir)){
         # skip directories that have results in them
         message("Skipping ", dir, " since it contains a Report.sso file and skipfinished=TRUE")
@@ -115,12 +146,17 @@ run_SS_models <- function(dirvec = NULL,
                      con = 'console.output.txt')
           message("console output written to console.output.txt")
         }
-        results[idir] <- "ran model"
+        if(isTRUE(console.output > 0)) {
+          results[idir] <- "model run failed"
+        } else if (isTRUE(console.output == 0)){
+          results[idir] <- "ran model"
+        } else {
+          results[idir] <- "unknown run status"
+        }
         setwd(wd_orig) # needed when using relative paths
       } # end model run
     } # end code for exe present
   } # end loop over directories
-
   # return table of results
   return(data.frame(dir=dirvec, results=results))
 }
