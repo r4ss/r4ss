f262fffc8758432762ea0801db80509abd62e2d8
kathryn.doering <kathryn.doering@noaa.gov>
2020-11-23 11:54:15 -0800

    #442 dev: get rm_dollar_sign fxn working

--- R/rm_dollar_sign.R
+++ R/rm_dollar_sign.R
@@ -2,80 +2,70 @@
 #' 
 #' The dollar sign is convienent to write, but allows for partial matching,
 #' which we don't often want. This function takes a file and changes all dollar
-#' signs to double brackets instead.
+#' signs to double brackets with names in quotations instead. Note that this 
+#' function will incorrectly convert text that includesnames in backticks that 
+#' include a dollar sign (for example, test$`my$name` will not convert correctly).
 #' @param file A file containing R source code
 #' @param out_file The name or path of a new file to write to. This is by
-#' default the same as the original file.
+#'  default the same as the original file. Set to NULL to avoid writing a new
+#'  file.
+#' @param allow_recursive Should dollar sign references of list in list be
+#'  replaced? If this is FALSE, then only the first reference will be replaced.
+#'  For example, F$first$second would become F[["first"]]$second when 
+#'  alow_recursive is FALSE, but would become F[["first"]][["second"]] if TRUE.
+#' @param max_loops How many times should dollar signs be looked for
+#'  recursively, if allow_recursive is TRUE? Defaults to 5. This is meant to
+#'  prevent an infinite loop in case the function does not work properly.
+#' @return A character vector of the modified text. As a side effect, produces
+#'  a text file out_file that was written out from R using writeLines().
 #' @author Kathryn Doering
+#' @example 
+#'  new_text <- rm_dollar_sign(
+#'    file = system.file("inst/extdata/test_rm_dollar_sign.txt",
+#'      package = "r4ss"),
+#'    out_file = NULL)
 
-rm_dollar_sign <- function(file = "inst/extdata/test_rm_dollar_sign.txt", out_file = "new_test_file.R") {
+rm_dollar_sign <- function(file = "inst/extdata/test_rm_dollar_sign.txt",
+                           out_file = file, 
+                           allow_recursive = TRUE,
+                           max_loops = 5) {
   #read in the file
   lines <- readLines(file)
-  # find starting position and length of all matches. Do this separately for
-  # synactic and nonsynactic names
-  matches_syn <- gregexpr(
-      pattern = "\\S\\$[[:alnum:]]+([[:alnum:]]|\\.|\\_)*(\\s|[[:punct:]]|$)",
-      text = lines)
-  matches_nonsyn <- gregexpr("\\S\\$`[[:print:]]+`", lines)
-  # merge the match lists, keeping the match.length attribute
-  merge_matches <- function(x, y) {
-    new_attr <- c(attr(x, "match.length"), attr(y, "match.length"))
-    new <- c(x, y)
-    attr(new, "match.length") <- new_attr 
-    new
-  }
-  matches_all <-  mapply(merge_matches,
-                         x = matches_syn, 
-                         y = matches_nonsyn,
-                         SIMPLIFY = FALSE)
-  for(i in seq_along(lines)) {
-    # skip the line if there are no matches
-    if(all(matches_all[[i]] == -1)) {
-      next
+
+  # get rid of names in back ticks first:
+  mod_lines <- gsub(pattern = "([[:alnum:]]|\\.|\\_)\\$`([[:print:]]+)`", 
+                    replacement = "\\1\\[\\[\"\\2\"\\]\\]", 
+                    lines)
+  # all others not in back ticks
+  pattern_no_backtick <- 
+    "([[:alnum:]]|\\.|\\_)\\$([[:alnum:]]+)(([[:alnum:]]|\\.|\\_)*)(\\s|[[:punct:]]|$)"
+  replace_no_backtick <- "\\1\\[\\[\"\\2\\3\"\\]\\]\\5"
+  mod_lines <- gsub(
+    pattern = pattern_no_backtick, 
+    replacement = replace_no_backtick, 
+    mod_lines)
+  if (allow_recursive) {
+    #get rid of $ when there are lists in lists.
+    ind  <- 1
+    while(length(grep(pattern_no_backtick, x = mod_lines)) > 0 &
+          ind <= max_loops) {
+      ind <- ind + 1
+      mod_lines <- gsub(pattern = pattern_no_backtick, 
+                        replacement = replace_no_backtick, 
+                        mod_lines)
     }
-    # get rid of -1's
-    tmp_matches <- matches_all[[i]]
-    to_rm <- which(matches_all == -1)
-    tmp_matches <- matches_all[-to_rm]
-    attr(tmp_matches, "match.length") <-
-      attr(tmp_matches, "match.length")[-to_rm]
-    
-    # Break up the lines, this part still needs work. One way to do this:
-    # 1. break the line up. have a key iding if each section needs to be 
-    # changed or not.
-    # 2. For each section to change, replace $name with [["name"]]
-    # 3. piece the line back together
-    # 4. replace the line in the original section
-    
-    # Some code that may be usable, but also may not be correct. based on an 
-    # approach of breaking upt the lines, but possibly including some chars not
-    # to modify in each of the substrings.
-    # line_broken <- substring(lines[i], 
-    #                 first = tmp_matches,
-    #                 last = c((tmp_matches[-1]), nchar(lines[i])))
-    # new_line_parts <- lapply(to_change, function(l) {
-    #   # find start and length in each section
-    #   matches_syn <- gregexpr("\\S\\$[[:alnum:]]+([[:alnum:]]|\\.|\\_)*(\\s|$)",
-    #   l)
-    #   matches_nonsyn <- gregexpr("\\S\\$`[[:print:]]+`", l)
-    #   matches_all <-  mapply(merge_matches,
-    #                          x = matches_syn,
-    #                          y = matches_nonsyn,
-    #                          SIMPLIFY = FALSE)
-    #   # check working properly
-    #   if(length(matches_all[[1]] != 1 & length(matches_all[[2]] != 1))) {
-    #     stop("function not working properly. rewrite code.")
-    #   }
-    #   # replace the parts necessary
-    #   # add [[]]
-    #   l <- sub("\\$", "\\[\\[\\'", l)
-    #   l <- sub("\\s|$", "'\\]\\]", l)
-    #   l
-    # })
-    # # put together line
-    # new_line <- paste0(c(line_broken[1], unlist(new_line_parts)))
-    # lines[i] <- new_line
+    if (length(grep(pattern_no_backtick, x = mod_lines)) > 0) {
+      warning("max_loops was set too low to replace all instances of dollar ",
+              "sign references.")
+    }
+  } else {
+    if (length(grep(pattern_no_backtick, x = mod_lines)) > 0) {
+      warning("There are lists in lists, but allow_recursive = FALE, so not all", 
+              "dollar sign operators were converted.")
+    }
   }
-  # read out the file to out_file
-  lines
+  if(!is.null(out_file)) {
+    writeLines(mod_lines, out_file)
+  }
+  mod_lines
 }
