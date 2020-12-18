a0fb81d9866007344c717a21ed893e14788102d1
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-12-18 07:26:44 -0800

    fix(SS_readctl_3.30): pass verbose and echoall to SS_readdat
    information was being printed to the screen with verbose = FALSE
    when passing a filename for the dat file rather than a list object.
    now, the verbose and echoall arguments are passed from the readctl
    function to the readdat function.

--- R/SS_readctl_3.30.R
+++ R/SS_readctl_3.30.R
@@ -234,7 +234,9 @@ SS_readctl_3.30 <- function(file, verbose = TRUE, echoall = FALSE, version = "3.
 
     ctllist[["fleetnames"]] <- fleetnames
   } else {
-    if (is.character(datlist)) datlist <- SS_readdat(file = datlist)
+    if (is.character(datlist)) {
+      datlist <- SS_readdat(file = datlist, verbose = verbose, echoall = echoall)
+    }
     if (is.null(datlist)) stop("datlist from SS_readdat is needed if use_datlist is TRUE")
     ctllist[["nseas"]] <- nseas <- datlist[["nseas"]]
     ctllist[["N_areas"]] <- N_areas <- datlist[["N_areas"]]
