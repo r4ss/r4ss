6dc71c9b2a7d331d2ce176799e3ba1fa3ae27852
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-11-18 12:19:23 -0800

    test(SS_read): test SS_read works with defaults
    part of #563

--- tests/testthat/test-readwrite.R
+++ tests/testthat/test-readwrite.R
@@ -14,6 +14,8 @@ test_that("models can be read and written", {
   message("Will read and write models", basename(all_mods))
   for (m in all_mods) {
     message("Now reading model ", basename(m))
+
+    #### Checks related to SS_read* functions
     start <- SS_readstarter(file = file.path(m, "starter.ss"), verbose = FALSE)
     fore <- SS_readforecast(file.path(m, "forecast.ss"), verbose = FALSE)
     dat <- SS_readdat(file.path(m, start[["datfile"]]), verbose = FALSE)
@@ -22,10 +24,22 @@ test_that("models can be read and written", {
     expect_true(is.list(fore))
     expect_true(is.list(dat))
     expect_true(is.list(ctl))
-    files <- file.path(m, c(
-      "starter.ss", "forecast.ss", start[["datfile"]],
-      start[["ctlfile"]]
+
+    #### Checks related to SS_read
+    allfiles <- SS_read(m)
+    # Check that the dir call was added to the list
+    expect_equal(m, allfiles[["dir"]])
+    # Check that the core names are in the list with input files
+    expect_true(all(
+      c("dat", "ctl", "start", "fore") %in% names(allfiles)
     ))
+    # Check that the length of the starter list matches
+    # the length of the native starter object
+    expect_equal(length(start), length(allfiles[["start"]]))
+
+    #### Checks related to writing files
+    files <- file.path(m, c("starter.ss", "forecast.ss", start[["datfile"]], 
+                            start[["ctlfile"]]))
     lapply(files, function(x) file.remove(x))
     message("Now writing model ", basename(m))
     SS_writestarter(start, dir = m, verbose = FALSE)
