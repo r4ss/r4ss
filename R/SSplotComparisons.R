3e3453400dffa24eab27fe63150b54ad2f7dfbd4
Ian Taylor <ian.taylor@noaa.gov>
2022-02-11 16:07:27 -0800

    use more roxygen templates

--- R/SSplotComparisons.R
+++ R/SSplotComparisons.R
@@ -100,13 +100,10 @@
 #' or a set of integers corresponding to the choice of models.
 #' @param shadealpha Transparency adjustment used to make default shadecol
 #' values (implemented as `adjustcolor(col=col, alpha.f=shadealpha)`)
-#' @param legend Add a legend?
+#' @template legend 
 #' @param legendlabels Optional vector of labels to include in legend. Default
 #' is 'model1','model2',etc.
-#' @param legendloc Location of legend. Either a string like "topleft" or a
-#' vector of two numeric values representing the fraction of the maximum in
-#' the x and y dimensions, respectively. See ?legend for more info on the
-#' string options.
+#' @template legendloc 
 #' @param legendorder Optional vector of model numbers that can be used to have
 #' the legend display the model names in an order that is different than that
 #' which is represented in the summary input object.
