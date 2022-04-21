2a606ae003fdbe386a3d264dba27d3ae285273bb
Ian Taylor <ian.taylor@noaa.gov>
2022-04-21 08:37:55 -0700

    doc(r4ss-package): add "_PACKAGE" #658

--- R/r4ss-package.R
+++ R/r4ss-package.R
@@ -7,10 +7,10 @@
 #' parameterizations, and various other tasks.
 #'
 #' \tabular{ll}{ Package: \tab r4ss\cr Type: \tab Package\cr Version: \tab
-#' 1.43.1\cr Date: \tab 2022-02-18\cr License: \tab GPL-3\cr LazyLoad: \tab
+#' 1.43.2\cr Date: \tab 2022-04-06\cr License: \tab GPL-3\cr LazyLoad: \tab
 #' yes\cr URL: \tab <https://github.com/r4ss/>\cr }
 #' Should be compatible with Stock Synthesis versions 3.24 through 3.30
-#' (specifically version 3.30.19beta, from February 2022).
+#' (specifically version 3.30.19.01, from April 2022).
 #'
 #' @name r4ss-package
 #' @aliases r4ss-package r4ss
@@ -54,4 +54,5 @@
 #' SS_plots(replist = myreplist)
 #' }
 #'
-NULL
+#' @keywords internal
+"_PACKAGE"
