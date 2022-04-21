9d3cf9947cdda1a8967d4a28440dfb7d1600f08d
Ian Taylor <ian.taylor@noaa.gov>
2022-04-21 11:51:47 -0700

    doc: improve author list in DESCRIPTION #658

--- R/r4ss-package.R
+++ R/r4ss-package.R
@@ -1,14 +1,3 @@
-#' @docType package
-#' @author Ian G. Taylor, Ian J. Stewart, Allan C. Hicks, Tommy M. Garrison,
-#' Andre E. Punt, John R. Wallace, Chantel R. Wetzel, James T. Thorson,
-#' Yukio Takeuchi, Kotaro Ono, Cole C. Monnahan, Christine C. Stawitz,
-#' Z. Teresa A'mar, Athol R. Whitten, Kelli F. Johnson, Robbie L. Emmet,
-#' Sean C. Anderson, Gwladys I. Lambert, Megan M. Stachura,
-#' Andrew B. Cooper, Andi Stephens, Neil L. Klaer, Carey R. McGilliard,
-#' Iago Mosqueira, Watal M. Iwasaki, Kathryn L. Doering, Andrea M. Havron,
-#' Nathan Vaughan, LaTreese S. Denson, Ashleigh J. Novak, and Henning Winker
-#'
-#' Package maintainer: Ian G. Taylor <Ian.Taylor@@noaa.gov>
 #' @import coda
 #' @import kableExtra
 #' @importFrom corpcor pseudoinverse
