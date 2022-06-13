562719fc208757717c1ad6c8732e147a97353310
Kathryn Doering <kathryn.doering@noaa.gov>
2022-06-13 12:53:48 -0400

    fix read URL test

--- tests/testthat/test-readwrite.R
+++ tests/testthat/test-readwrite.R
@@ -84,9 +84,11 @@ test_that("empty files lead to NULL or error", {
   ))
 })
 
-test_that("ss_read works with a URL", {
+test_that("ss_read works with a raw github URL", {
   skip_if_offline(host = "github.com")
   list_objs <- SS_read(dir = 
-  "https://github.com/nmfs-stock-synthesis/test-models/tree/main/models/Simple")
-  expect_vector(list_objs, ptype = "list")
+  "https://raw.githubusercontent.com/nmfs-stock-synthesis/test-models/main/models/Simple")
+  expect_true(is.list(list_objs))
+  expect_equal(names(list_objs), c("dir", "path", "dat", "ctl", "start", "fore",
+   "wtatage"))
 })
