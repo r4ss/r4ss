0c4fd54efa8d73ae9a28adefea31be676f94cbc6
Kathryn Doering <kathryn.doering@noaa.gov>
2022-06-16 12:53:15 -0400

    fix: docs for pheight

--- man-roxygen/pwidth_pheight.R
+++ man-roxygen/pwidth_pheight.R
@@ -1,5 +1,5 @@
 #' @param pwidth Default width of plots printed to files in units of
 #' \code{punits}. The default is \code{pwidth=6.5}.
-#' #' @param pheight Height of plots printed to png files in units of `punits`.
+#' @param pheight Height of plots printed to png files in units of `punits`.
 #' Default is designed to allow two plots per page, with `pheight_tall` used
 #' for plots that work best with a taller format and a single plot per page.
