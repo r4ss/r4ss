c428d9bc1c4c5ee31df2485c06968b4061b2da4a
Ian Taylor <ian.taylor@noaa.gov>
2022-08-03 16:20:51 -0700

    deprecate run() working on dir as vector #733

--- R/run.R
+++ R/run.R
@@ -5,8 +5,7 @@
 #' `dir` is a vector, it iteratively runs the SS3 model in each one.
 #' Parallelization is not available at this time.
 #'
-#' @param dir Directory, or vector of directories, containing the
-#' model input files.
+#' @param dir Directory containing the model input files.
 #' @template exe
 #' @template extras
 #' @param skipfinished Skip any folders that already contain a Report.sso file.
@@ -16,10 +15,8 @@
 #' show_in_console = FALSE)
 #' @template verbose
 #'
-#' @return Returns a table showing which directories had models run and which
-#' had errors like missing executable or Report.sso already present.
-#' The three possible messages are "model run failed", "ran model", and
-#' "unknown run status".
+#' @return Returns one of three messages: 
+#' "model run failed", "ran model", and "unknown run status".
 #'
 #' @description The `run()` function checks for the executable via
 #' [r4ss::check_exe()]. This involves first checking the
@@ -60,6 +57,13 @@ run <- function(dir = getwd(),
   if (!show_in_console & !is.character(console_output_file)) {
     stop("Input 'console_output_file' should be a character string")
   }
+  if (length(dir) > 1) {
+    lifecycle::deprecate_stop(
+      when = "1.46.2",
+      what = "run(dir = 'must be a single directory')",
+      details = "Please use the {purrr} package or other tools to apply run() to multiple directories"
+    )
+  }
 
   wd_orig <- getwd()
   on.exit(setwd(wd_orig), add = TRUE)
@@ -74,82 +78,75 @@ run <- function(dir = getwd(),
   check_exe_results <- check_exe(dir = dir, exe = exe, verbose = verbose)
   command <- check_exe_results[["exe"]]
 
-  # loop over directories
-  for (idir in seq_along(dir)) {
-    # directory where stuff will happen
-    dir <- dir[idir]
-
-    # confirm that dir exists
-    if (!dir.exists(dir[idir])) {
-      warning("not a directory:", dir[idir])
-      results[idir] <- "not a directory"
+  # confirm that dir exists
+  if (!dir.exists(dir)) {
+    warning("not a directory:", dir)
+    results <- "not a directory"
+  } else {
+    if (skipfinished & "Report.sso" %in% dir(dir)) {
+      # skip directories that have results in them
+      message(
+        "Skipping ", dir, " since it contains",
+        " a Report.sso file and skipfinished = TRUE"
+      )
+      results <- "contained Report.sso"
     } else {
-      if (skipfinished & "Report.sso" %in% dir(dir[idir])) {
-        # skip directories that have results in them
+      # run model
+      if (verbose) {
+        message("changing working directory to ", dir)
+      }
+      setwd(dir) # change working directory
+      if (verbose) {
         message(
-          "Skipping ", dir[idir], " since it contains",
-          " a Report.sso file and skipfinished = TRUE"
+          "Running model in directory: ", getwd(), "\n",
+          "Using the command: ", command, " ", extras
         )
-        results[idir] <- "contained Report.sso"
-      } else {
-        # run model
-        if (verbose) {
-          message("changing working directory to ", dir[idir])
-        }
-        setwd(dir[idir]) # change working directory
-
+      }
+      if (!show_in_console) {
         if (verbose) {
           message(
-            "Running model in directory: ", getwd(), "\n",
-            "Using the command: ", command, " ", extras
+            "Input 'show_in_console' = FALSE, ",
+            "so writing console output to ",
+            console_output_file
           )
         }
-        if (!show_in_console) {
-          if (verbose) {
-            message(
-              "Input 'show_in_console' = FALSE, ",
-              "so writing console output to ",
-              console_output_file
-            )
-          }
-        }
-        console_output <- system2(command,
-          args = extras,
-          stdout = ifelse(show_in_console,
-            "",
-            TRUE
-          ),
-          stderr = ifelse(show_in_console,
-            "",
-            TRUE
-          )
+      }
+      console_output <- system2(command,
+        args = extras,
+        stdout = ifelse(show_in_console,
+          "",
+          TRUE
+        ),
+        stderr = ifelse(show_in_console,
+          "",
+          TRUE
         )
-        if (!show_in_console) {
-          writeLines(c(
-            "###",
-            "console output",
-            as.character(Sys.time()),
-            "###",
-            " ",
-            console_output
-          ),
-          con = console_output_file
-          )
-          if (verbose) {
-            message("console output written to ", console_output_file)
-          }
+      )
+      if (!show_in_console) {
+        writeLines(c(
+          "###",
+          "console output",
+          as.character(Sys.time()),
+          "###",
+          " ",
+          console_output
+        ),
+        con = console_output_file
+        )
+        if (verbose) {
+          message("console output written to ", console_output_file)
         }
-        if (isTRUE(console_output > 0)) {
-          results[idir] <- "model run failed"
-        } else if (isTRUE(console_output == 0)) {
-          results[idir] <- "ran model"
-        } else {
-          results[idir] <- "unknown run status"
-        }
-        setwd(wd_orig) # needed when using relative paths
-      } # end model run
-    } # end code for exe present
-  } # end loop over directories
-  # return table of results
-  return(data.frame(dir = dir, results = results))
+      }
+      if (isTRUE(console_output > 0)) {
+        results <- "model run failed"
+      } else if (isTRUE(console_output == 0)) {
+        results <- "ran model"
+      } else {
+        results <- "unknown run status"
+      }
+      setwd(wd_orig) # needed when using relative paths
+    } # end model run
+  } # end code for exe present
+
+  return(results = results)
 }
