7ec4300084bf73327f804089642819f3d591b906
Ian Taylor <ian.taylor@noaa.gov>
2023-02-08 15:25:12 -0800

    refactor: remove redundant  code for areacols

--- R/SSplotNumbers.R
+++ R/SSplotNumbers.R
@@ -27,7 +27,7 @@
 #' setup) is thousands (numbers.unit=1000).
 #' @param areas optional subset of areas to plot for spatial models
 #' @param areanames names for areas. Default is to use Area1, Area2,...
-#' @param areacols vector of colors by area
+#' @template areacols
 #' @param pntscalar maximum bubble size for bubble plots; each plot scaled
 #' independently based on this maximum size and the values plotted. Often some
 #' plots look better with one value and others with a larger or smaller value.
@@ -59,7 +59,7 @@ SSplotNumbers <-
            numbers.unit = 1000,
            areas = "all",
            areanames = "default",
-           areacols = "default",
+           areacols = NULL,
            pntscalar = 2.6,
            bub.bg = gray(0.5, alpha = 0.5),
            bublegend = TRUE,
@@ -139,10 +139,8 @@ SSplotNumbers <-
         areanames <- paste0("area", 1:nareas)
       }
 
-      if (areacols[1] == "default") {
-        areacols <- rich.colors.short(nareas)
-        if (nareas > 2) areacols <- rich.colors.short(nareas + 1)[-1]
-      }
+      # set default area-specific colors if not specified
+      areacols <- get_areacols(areacols, nareas)
 
       if (SS_versionNumeric <= 3.1) {
         stop("numbers at age plots no longer supported for SS version 3.10 and earlier")
