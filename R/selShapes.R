2809c251ee89d035b47b5c27842a57ad9d7cd9bc
Ian Taylor <ian.taylor@noaa.gov>
2023-03-17 13:44:03 -0700

    update URL in selShapes() #788

--- R/selShapes.R
+++ R/selShapes.R
@@ -1,10 +1,10 @@
 #' Launch a shiny app that displays various selectivity curves
 #'
-#' This app is hosted at https://connect.fisheries.noaa.gov/shiny-selex-ss3/
+#' This app is hosted at https://connect.fisheries.noaa.gov/ss3-helper/
 #' @export
-#' @author Allan Hicks, Andrea Havron, Ian Taylor,
+#' @author Allan Hicks, Andrea Havron, Ian Taylor
 #' @author inspired by tcl/tk code written by Tommy Garrison
 
 selShapes <- function() {
-  browseURL(url = "https://connect.fisheries.noaa.gov/shiny-selex-ss3/")
+  browseURL(url = "https://connect.fisheries.noaa.gov/ss3-helper/")
 }
