710b23ec711933d63d7e081de2b4d24e95dbc3fb
Ian Taylor <ian.taylor@noaa.gov>
2023-05-30 16:04:40 -0700

    import only select functions from {kableExtra}
    - relates to https://github.com/PIFSCstockassessments/ss3diags/issues/82
    - relates to
    https://github.com/PIFSCstockassessments/ss3diags/issues/83

--- R/r4ss-package.R
+++ R/r4ss-package.R
@@ -1,6 +1,5 @@
 #' @import coda
 #' @import dplyr
-#' @import kableExtra
 #' @import magrittr
 #' @importFrom corpcor pseudoinverse
 #' @importFrom grDevices adjustcolor col2rgb colorRampPalette
@@ -11,6 +10,7 @@
 #'   legend lines matplot mtext pairs par
 #'   persp plot points polygon rect segments
 #'   symbols text title
+#' @importFrom kableExtra cell_spec kable kable_styling scroll_box 
 #' @importFrom stats acf aggregate density dnorm loess
 #'   median na.omit nlminb optim pnorm qbeta qchisq qlnorm
 #'   qnorm qt quantile rnorm sd var window
