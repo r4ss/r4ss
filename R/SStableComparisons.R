178beee8b7928a7a631f6d260a6857d1e5780b87
Ian Taylor <ian.taylor@noaa.gov>
2023-06-22 09:29:58 -0700

    fix: tables keep SSB val/label when in numbers

--- R/SStableComparisons.R
+++ R/SStableComparisons.R
@@ -112,7 +112,12 @@ SStableComparisons <- function(summaryoutput,
             vals[1, 1] <- paste0(vals[1, 1], "_thousands")
           }
         }
-        if (substring(name, 1, 3) %in% c("SPB", "SSB") | substring(name, 1, 8) == "TotYield") {
+        if (substring(name, 1, 8) == "TotYield") {
+          vals[1, -1] <- round(vals[1, -1] / 1e3, 3)
+          vals[1, 1] <- paste0(vals[1, 1], "_thousand_mt")
+        }
+        if (substring(name, 1, 3) %in% c("SPB", "SSB") & 
+          all(summaryoutput[["SpawnOutputUnits"]] == "biomass")) {
           vals[1, -1] <- round(vals[1, -1] / 1e3, 3)
           vals[1, 1] <- paste0(vals[1, 1], "_thousand_mt")
         }
@@ -182,9 +187,14 @@ SStableComparisons <- function(summaryoutput,
             vals[1, 1] <- paste0(vals[1, 1], "_thousands")
           }
         }
-        if (substring(name, 1, 3) %in% c("SPB", "SSB") | substring(name, 1, 8) == "TotYield") {
+        if (substring(name, 1, 8) == "TotYield") {
           vals[1, -1] <- round(vals[1, -1] / 1e3, 3)
-          vals[1, 1] <- paste(vals[1, 1], "thousand_mt", sep = "_")
+          vals[1, 1] <- paste0(vals[1, 1], "_thousand_mt")
+        }
+        if (substring(name, 1, 3) %in% c("SPB", "SSB") & 
+          all(summaryoutput[["SpawnOutputUnits"]] == "biomass")) {
+          vals[1, -1] <- round(vals[1, -1] / 1e3, 3)
+          vals[1, 1] <- paste0(vals[1, 1], "_thousand_mt")
         }
         if (!is.null(digits)) {
           if (verbose) message("rounded to", digit, "digits")
