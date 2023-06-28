4741e9921210974cfc3b2a869125c0c0372a420a
e-gugliotti-NOAA <elizabeth.gugliotti@noaa.gov>
2023-06-28 17:19:34 -0400

    fix test for get_ss3_exe()

--- tests/testthat/test-get_ss3_exe.R
+++ tests/testthat/test-get_ss3_exe.R
@@ -1,8 +1,8 @@
-test_that("executables are downloading"){
+test_that("executables are downloading", {
   download_loc <- get_ss3_exe()
   download_filepath <- gsub(".*: ","", download_loc)
   exe_name <- gsub(paste0(getwd(),"/"),"", download_filepath)
   dir_wd <- file.path(getwd(),exe_name)
   
   testthat::expect_equal(dir_wd, download_filepath)
-}
+})
