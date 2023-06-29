2df4282dd2f616b4233a4572fab60baaa3f122d2
e-gugliotti-NOAA <elizabeth.gugliotti@noaa.gov>
2023-06-29 13:06:17 -0400

    try another testthat fix

--- tests/testthat/test-get_ss3_exe.R
+++ tests/testthat/test-get_ss3_exe.R
@@ -1,3 +1,4 @@
+example_path <- system.file("extdata", package = "r4ss")
 temp_path <- file.path(tempdir(), "test_ss3_exe")
 dir.create(temp_path, showWarnings = FALSE)
 # remove all artifacts created from testing. (developers: simply comment out
@@ -5,10 +6,10 @@ dir.create(temp_path, showWarnings = FALSE)
 on.exit(unlink(temp_path, recursive = TRUE), add = TRUE)
 
 test_that("executables are downloading", {
-  download_loc <- get_ss3_exe(dir = temp_path)
+  download_loc <- get_ss3_exe(dir = example_path)
   download_filepath <- gsub(".*: ","", download_loc)
-  exe_name <- gsub(".*test_ss3_exe/","", download_filepath)
-  dir_temp <- file.path(temp_path, exe_name)
+  exe_name <- gsub(paste0(example_path,"/"),"", download_filepath)
+  dir_temp <- file.path(example_path, exe_name)
   
   expect_equal(dir_temp, download_filepath)
 })
