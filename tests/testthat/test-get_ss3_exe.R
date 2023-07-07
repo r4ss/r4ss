b5857435152b21cdd8141381068dcdeff1d78145
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-07-07 15:07:22 -0400

    add get_ss_exe() function (#839)
    * add get_ss3_exe() function and test
    * add get_ss3_exe() to vignette
    * update wordlist
    * add .RData to .gitignore

---
+++ tests/testthat/test-get_ss3_exe.R
@@ -0,0 +1,26 @@
+# example_path <- system.file("extdata", package = "r4ss")
+temp_path <- file.path(tempdir(), "test_ss3_exe")
+dir.create(temp_path, showWarnings = FALSE)
+# remove all artifacts created from testing. (developers: simply comment out
+# the line below if you want to keep artifacts for troubleshooting purposes)
+on.exit(unlink(temp_path, recursive = TRUE), add = TRUE)
+
+test_that("executables are downloading default latest version", {
+  download_loc <- get_ss3_exe(dir = temp_path)
+  download_filepath <- gsub(".*: ","", download_loc)
+  exe_name <- gsub(paste0(temp_path,"/"),"", download_filepath, fixed = TRUE)
+  dir_temp <- file.path(temp_path, exe_name)
+  file.remove(download_filepath)
+  
+  expect_equal(dir_temp, download_filepath)
+})
+
+test_that("executables are downloading with version", {
+  download_loc <- get_ss3_exe(dir = temp_path, version = "v3.30.18")
+  download_filepath <- gsub(".*: ","", download_loc)
+  exe_name <- gsub(paste0(temp_path,"/"),"", download_filepath, fixed = TRUE)
+  dir_temp <- file.path(temp_path, exe_name)
+  file.remove(download_filepath)
+  
+  expect_equal(dir_temp, download_filepath)
+})
