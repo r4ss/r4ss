5dfab8371c8ace6f8bd8a5909a2d96462dda2967
Ian Taylor <ian.taylor@noaa.gov>
2024-01-04 16:06:27 -0800

    support for DM likelihood in size comp data #898

--- R/SS_output.R
+++ R/SS_output.R
@@ -509,7 +509,7 @@ SS_output <-
     logfile_name <- dir(dir, pattern = ".log$")
     logfile_name <- logfile_name[logfile_name != "fmin.log"]
     if (length(logfile_name) > 1) {
-      filetimes <- file.info(file.path(dir, logfile))$mtime
+      filetimes <- file.info(file.path(dir, logfile_name))$mtime
       logfile_name <- logfile_name[filetimes == max(filetimes)]
       if (verbose) {
         message(
@@ -626,6 +626,7 @@ SS_output <-
     # placeholders for tables added in 3.30.21
     Length_comp_error_controls <- NULL
     Age_comp_error_controls <- NULL
+    Size_comp_error_controls <- NULL
 
     if ("Jitter:" %in% rawdefs[["X1"]]) {
       get.def <- function(string) {
@@ -711,6 +712,7 @@ SS_output <-
       seasfracs <- seasfracs - seasdurations / 2 # should be mid-point of each season as a fraction of the year
 
       # end DEFINITIONS elements in 3.30.12-3.30.20
+
       if ("Length_comp_error_controls" %in% rawdefs[["X1"]]) {
         # read table of length comp error controls (added 3.30.21)
         Length_comp_error_controls <-
@@ -752,6 +754,16 @@ SS_output <-
         # remove extra column with hash symbols
         Age_comp_error_controls <- Age_comp_error_controls %>%
           dplyr::select(-NoName)
+      }
+
+      if ("Size_comp_error_controls" %in% rawdefs[["X1"]]) {
+        # read table of age comp error controls (added 3.30.21)
+        Size_comp_error_controls <-
+          match_report_table("Size_comp_error_controls",
+            adjust1 = 1,
+            header = TRUE, type.convert = TRUE
+          ) %>%
+          dplyr::rename(Sz_method = "#_Sz_method") # remove hash from header
       }
       # end read of 3.30.12+ DEFINITIONS
     } else {
@@ -2288,20 +2300,28 @@ SS_output <-
               dplyr::select(Fleet, Time, Sexes, Part, Nsamp_DM) %>%
               dplyr::left_join(lendbase, .)
           }
-          # repeat for other parts of CompReport.sso
+          # add info to age comp data
           if (nrow(agedbase) > 0) {
             agedbase <- fit_age_comps %>%
               dplyr::rename(Like_sum = Like) %>% # like for vector not bin
               dplyr::select(Fleet, Time, Sexes, Part, Nsamp_DM) %>%
               dplyr::left_join(agedbase, .)
           }
+          # add info to conditional age-at-length comp data
           if (nrow(condbase) > 0) {
             condbase <- fit_age_comps %>%
               dplyr::rename(Like_sum = Like) %>% # like for vector not bin
               dplyr::select(Fleet, Time, Sexes, Part, Nsamp_DM) %>%
               dplyr::left_join(condbase, .)
           }
-          # IGT 28 Jan 2023: need to add support for DM for generalized size comps
+          # add info to generalized size comp data
+          if (nrow(sizedbase) > 0) {
+            sizedbase <- fit_size_comps %>%
+              dplyr::rename(Like_sum = Like) %>% # like for vector not bin
+              dplyr::rename(method = Method) %>% # making it match what's in sizedbase
+              dplyr::select(Fleet, Time, Sexes, Part, Nsamp_DM, method) %>%
+              dplyr::left_join(sizedbase, .)
+          }
         } # end test for whether CompReport.sso info is available
         # end approach used starting in 3.30.21
       } else {
@@ -2546,6 +2566,7 @@ SS_output <-
     returndat[["ALK_tolerance"]] <- return.def("ALK_tolerance")
     returndat[["Length_comp_error_controls"]] <- Length_comp_error_controls
     returndat[["Age_comp_error_controls"]] <- Age_comp_error_controls
+    returndat[["Size_comp_error_controls"]] <- Size_comp_error_controls
     returndat[["nforecastyears"]] <- nforecastyears
     returndat[["morph_indexing"]] <- morph_indexing
     returndat[["MGparmAdj"]] <- MGparmAdj
