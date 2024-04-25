24e31c399a747ade9924e5c892ecab710efbedd1
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2024-04-25 15:08:52 -0400

    add m2 chip ss3 executable to get_ss3_exe() function (#936)
    * add m2 chip executable
    
    * update exe name to arm64
    
    * fix version for arm64
    
    * fix the other wrong release name
    
    * fix test that expect_true() for many true values, fix workflow to always output artifact

--- tests/testthat/test-runs.R
+++ tests/testthat/test-runs.R
@@ -143,7 +143,7 @@ test_that("jitter runs on simple_small model", {
     expect_equal(starter$jitter_fraction, 0)
   } else {
     run_results_jit_init <- run(dir = dir.jit)
-    expect_true(run_results_jit_init == "ran model")
+    expect_true(all(unlist(run_results_jit_init == "ran model")))
 
     likesaved <- jitter(
       dir = dir.jit, Njitter = 2, jitter_fraction = 0.1,
@@ -242,7 +242,7 @@ test_that("Run an SS3 model and read the hessian", {
   )
   expect_true(copy_results)
   run_results <- run(dir = file.path(tmp_path, "test_mod_run"))
-  expect_true(run_results == "ran model")
+  expect_true(all(unlist(run_results == "ran model")))
   hes <- getADMBHessian(
     hesfile = file.path(tmp_path, "test_mod_run", "admodel.hes")
   )
