a499112fb29a58a5763b82da5aaa6ac0e4c73d71
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2024-12-09 17:10:15 -0500

    try remove token argument to see if that works

--- R/get_ss3_exe.r
+++ R/get_ss3_exe.r
@@ -41,11 +41,13 @@ get_ss3_exe <- function(dir = NULL, version = NULL) {
 
   # Get latest release if version not specified
   if (is.null(version)) {
-    latest_release <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/releases/latest", page = 1, .token = token)
+    latest_release <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/releases/latest", page = 1)
+    # latest_release <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/releases/latest", page = 1, .token = token)
     tag <- latest_release[["tag_name"]]
   } else {
     # Otherwise get specified version
-    all_tags <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/tags", .token = token)
+    all_tags <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/tags")
+    # all_tags <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/tags", .token = token)
     df_tags <- as.data.frame(do.call(rbind, all_tags))
     tags <- unlist(df_tags[["name"]])
 
