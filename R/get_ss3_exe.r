6080596f681dc5301c65d01dd7a44977c344a422
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2024-12-10 12:31:19 -0500

    Provide a fix to use tokens for mac github actions runner for get_ss3_exe() (#986)
    * try fix to not use token as NA_character_ if running in github actions
    
    * fix for r4ss
    
    * try remove token argument to see if that works
    
    * try again with ifelse statement
    
    * add temp directory search for vignette usage of get_ss3_exe()

--- R/get_ss3_exe.r
+++ R/get_ss3_exe.r
@@ -24,13 +24,30 @@
 #' https://github.com/nmfs-ost/ss3-source-code/tags
 
 get_ss3_exe <- function(dir = NULL, version = NULL) {
+  if (is.null(dir)) {
+    dir <- getwd()
+    message("No directory provided, the executable will be downloaded to the working directory")
+  }
+
+  if (!dir.exists(dir)) {
+    stop("Directory doesn't exist: ", dir)
+  }
+
   # Get latest release if version not specified
   if (is.null(version)) {
-    latest_release <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/releases/latest", page = 1, .token = NA_character_)
+    if (dir == "/Users/runner/work/r4ss/r4ss" || grepl("/var/folders", dir)) {
+      latest_release <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/releases/latest", page = 1)
+    } else {
+      latest_release <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/releases/latest", page = 1, .token = NA_character_)
+    }
     tag <- latest_release[["tag_name"]]
   } else {
     # Otherwise get specified version
-    all_tags <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/tags", .token = NA_character_)
+    if (dir == "/Users/runner/work/r4ss/r4ss" || grepl("/var/folders", dir)) {
+      all_tags <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/tags")
+    } else {
+      all_tags <- gh::gh("GET /repos/nmfs-ost/ss3-source-code/tags", .token = NA_character_)
+    }
     df_tags <- as.data.frame(do.call(rbind, all_tags))
     tags <- unlist(df_tags[["name"]])
 
@@ -41,15 +58,6 @@ get_ss3_exe <- function(dir = NULL, version = NULL) {
     } else {
       tag <- version
     }
-  }
-
-  if (is.null(dir)) {
-    dir <- getwd()
-    message("No directory provided, the executable will be downloaded to the working directory")
-  }
-
-  if (!dir.exists(dir)) {
-    stop("Directory doesn't exist: ", dir)
   }
 
   if (.Platform[["OS.type"]] == "windows") {
