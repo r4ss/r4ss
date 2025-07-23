c810fac10f6467f9ae26dddb3a213f809754659f
iantaylor-NOAA <ian.taylor@noaa.gov>
2025-07-23 15:16:29 -0700

    further simplify sequential tests

--- tests/testthat/test-test-models.R
+++ tests/testthat/test-test-models.R
@@ -1,29 +1,21 @@
 context("Read output and make plots for all test-models")
 
-# temporary directory
-mod_path <- file.path(tempdir(check = TRUE), "test-test-models")
-download_models(dir = mod_path)
+# download models to a temporary directory
+models_path <- file.path(tempdir(check = TRUE), "test-test-models")
+download_models(dir = models_path)
 mods <- list.dirs(
-  file.path(mod_path, "models"),
+  file.path(models_path, "models"),
   full.names = FALSE,
   recursive = FALSE
 )
 
 # run models
-for (i in seq_along(all_mods)) {
+for (i in seq_along(mods)) {
   test_that(
     glue::glue(
       "run SS_output() and SS_plots() on test model: {mods[i]}"
     ),
     {
-      skip_if(
-        !file.exists(system.file("extdata", "models", package = "r4ss")),
-        message = "No 'models' folder in 'extdata'"
-      )
-      # skip if no executable in simple_small path
-      # (should have been loaded there by
-      # .github\workflows\r4ss-extra-tests.yml)
-
       # find simple_small
       dir_exe <- system.file("extdata", "simple_small", package = "r4ss")
       skip_if(
@@ -31,32 +23,25 @@ for (i in seq_along(all_mods)) {
           !file.exists(file.path(dir_exe, "ss3.exe"))),
         message = paste("skipping test: no exe called 'ss3' found in", dir_exe)
       )
-      # copy all test models to temporary directory
-      orig_mod_path <- system.file("extdata", "models", package = "r4ss")
-      orig_all_mods <- list.dirs(
-        file.path(mod_path, "models"),
-        full.names = TRUE,
-        recursive = FALSE
-      )
-      mod <- basename(orig_all_mods[i])
-      file.copy(orig_all_mods[i], file.path(mod_path, mod))
-
+      mod <- basename(mods[i])
+      mod_path <- file.path(models_path, "models", mod)
+      
       cli::cli_alert_info(
         "Now running without estimation: {mod}"
       )
       run(
-        all_mods[i],
+        mod_path,
         exe = file.path(dir_exe, "ss3"),
         extras = "-stopph 0 -nohess"
       )
 
-      if (!"Report.sso" %in% dir(all_mods[i])) {
-        cli::cli_alert_warning("No Report.sso file in {all_mods[i]}")
+      if (!"Report.sso" %in% dir(mod_path)) {
+        cli::cli_alert_warning("No Report.sso file in {mod_path}")
       } else {
         #### Checks related to SS_output()
         message("Running SS_output()")
         output <- SS_output(
-          all_mods[i],
+          mod_path,
           verbose = FALSE,
           printstats = FALSE
         )
@@ -67,15 +52,16 @@ for (i in seq_along(all_mods)) {
       cli::cli_alert_info(
         "Running SS_plots() for model {mod}"
       )
-      SS_plots(output, verbose = FALSE)
-      expect_true("data_plot2.png" %in% dir(file.path(all_mods[i], "plots")))
+      # make low-resolution plots to save time
+      SS_plots(output, verbose = FALSE, res = 50)
+      expect_true("data_plot2.png" %in% dir(file.path(mod_path, "plots")))
 
       cli::cli_alert_info(
         "Running table_all() for model {mod}"
       )
       table_all(output, verbose = TRUE)
       expect_true(
-        "table_parcounts.rda" %in% dir(file.path(all_mods[i], "tables"))
+        "table_parcounts.rda" %in% dir(file.path(mod_path, "tables"))
       )
     }
   ) # end testthat
