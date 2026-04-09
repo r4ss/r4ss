db266237d3bdd10d0397205b3eacec1fb66d3c50
iantaylor-NOAA <4992918+iantaylor-NOAA@users.noreply.github.com>
2026-04-09 19:33:23 +0000

    style and docs: run devtools::document() and style with styler or air

--- tests/testthat/test-ss3_data_to_fims.R
+++ tests/testthat/test-ss3_data_to_fims.R
@@ -1,66 +1,66 @@
 context("Convert SS3 data to FIMS format")
 
 test_that("ss3_data_to_fims() runs on simple_small", {
-    path <- system.file("extdata", "simple_small", package = "r4ss")
+  path <- system.file("extdata", "simple_small", package = "r4ss")
 
-    simple_small_input <- SS_read(dir = path)
+  simple_small_input <- SS_read(dir = path)
 
-    expect_no_error(
-        fims_data <- ss3_data_to_fims(ss3_dir = path)
-    )
+  expect_no_error(
+    fims_data <- ss3_data_to_fims(ss3_dir = path)
+  )
 })
 
 test_that("ss3_data_to_fims() output has expected structure and values", {
-    expect_named(
-        fims_data,
-        c(
-            "type",
-            "name",
-            "age",
-            "length",
-            "timing",
-            "value",
-            "unit",
-            "uncertainty"
-        )
+  expect_named(
+    fims_data,
+    c(
+      "type",
+      "name",
+      "age",
+      "length",
+      "timing",
+      "value",
+      "unit",
+      "uncertainty"
     )
+  )
 
-    expect_true(all(
-        c(
-            "landings",
-            "index",
-            "age_comp",
-            "length_comp",
-            "weight_at_age",
-            "age_to_length_conversion"
-        ) %in%
-            fims_data$type
-    ))
+  expect_true(all(
+    c(
+      "landings",
+      "index",
+      "age_comp",
+      "length_comp",
+      "weight_at_age",
+      "age_to_length_conversion"
+    ) %in%
+      fims_data$type
+  ))
 })
 
 test_that("ss3_data_to_fims() age-to-length conversion data has expected dimensions", {
-    years <- simple_small_input[["dat"]][["styr"]]:simple_small_input[["dat"]][[
-        "endyr"
-    ]]
-    length_bins <- simple_small_input[["dat"]][["lbin_vector"]]
-    age_bins <- 0:max(simple_small_input[["dat"]][["agebin_vector"]])
+  years <- simple_small_input[["dat"]][["styr"]]:simple_small_input[["dat"]][[
+    "endyr"
+  ]]
+  length_bins <- simple_small_input[["dat"]][["lbin_vector"]]
+  age_bins <- 0:max(simple_small_input[["dat"]][["agebin_vector"]])
 
-    age_to_length <- subset(fims_data, type == "age_to_length_conversion")
+  age_to_length <- subset(fims_data, type == "age_to_length_conversion")
 
-    expect_equal(nrow(age_to_length), length(length_bins) * length(age_bins))
+  expect_equal(nrow(age_to_length), length(length_bins) * length(age_bins))
 
-    expect_setequal(
-        object = unique(age_to_length$length),
-        expected = length_bins
-    )
-    expect_setequal(
-        object = unique(age_to_length$age),
-        expected = age_bins
-    )
+  expect_setequal(
+    object = unique(age_to_length$length),
+    expected = length_bins
+  )
+  expect_setequal(
+    object = unique(age_to_length$age),
+    expected = age_bins
+  )
 })
 
 test_that("ss3_data_to_fims() weight-at-age data has expected dimensions", {
-    # tests for converted weight-at-age data
-    weight_at_age <- subset(fims_data, type == "weight_at_age")
-    expect_equal(nrow(weight_at_age), length(age_bins) * (1 + length(years)))
+  # tests for converted weight-at-age data
+  weight_at_age <- subset(fims_data, type == "weight_at_age")
+  expect_equal(nrow(weight_at_age), length(age_bins) * (1 + length(years)))
 })
