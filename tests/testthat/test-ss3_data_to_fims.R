373879bb5c7031b81021e9ab1b9b1c0e82587776
iantaylor-NOAA <4992918+iantaylor-NOAA@users.noreply.github.com>
2026-04-09 18:36:55 +0000

    style and docs: run devtools::document() and style with styler or air

--- tests/testthat/test-ss3_data_to_fims.R
+++ tests/testthat/test-ss3_data_to_fims.R
@@ -21,21 +21,49 @@ test_that("ss3_data_to_fims() handles irregular population length bins", {
     )
   )
 
-  expect_true(all(c(
-    "landings",
-    "index",
-    "age_comp",
-    "length_comp",
-    "weight_at_age",
-    "age_to_length_conversion"
-  ) %in% fims_data$type))
+  expect_true(all(
+    c(
+      "landings",
+      "index",
+      "age_comp",
+      "length_comp",
+      "weight_at_age",
+      "age_to_length_conversion"
+    ) %in%
+      fims_data$type
+  ))
 
   age_to_length <- subset(fims_data, type == "age_to_length_conversion")
   expect_equal(nrow(age_to_length), 25 * 16)
-  expect_setequal(unique(age_to_length$length), c(
-    26, 28, 30, 32, 34, 36, 38, 40, 42, 44,
-    46, 48, 50, 52, 54, 56, 58, 60, 62, 64,
-    68, 72, 76, 80, 90
-  ))
+  expect_setequal(
+    unique(age_to_length$length),
+    c(
+      26,
+      28,
+      30,
+      32,
+      34,
+      36,
+      38,
+      40,
+      42,
+      44,
+      46,
+      48,
+      50,
+      52,
+      54,
+      56,
+      58,
+      60,
+      62,
+      64,
+      68,
+      72,
+      76,
+      80,
+      90
+    )
+  )
   expect_setequal(unique(age_to_length$age), 0:15)
 })
