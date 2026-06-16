dd7477ff8dfc9ffe6f0ab57233a612e00be59e27
iantaylor-NOAA <ian.taylor@noaa.gov>
2026-06-16 13:15:50 -0700

    update documentation

--- R/r4ss-package.R
+++ R/r4ss-package.R
@@ -3,21 +3,11 @@
 #' @import dplyr
 #' @import viridis
 #' @importFrom corpcor pseudoinverse
-#' @importFrom grDevices adjustcolor col2rgb colorRampPalette
-#'   dev.new dev.off graphics.off
-#'   gray grey pdf png rainbow rgb topo.colors
-#' @importFrom graphics abline arrows axis barplot box
-#'   contour curve grid hist image layout
-#'   legend lines matplot mtext pairs par
-#'   persp plot points polygon rect segments
-#'   symbols text title
+#' @importFrom grDevices adjustcolor col2rgb colorRampPalette dev.new dev.off graphics.off gray grey pdf png rainbow rgb topo.colors
+#' @importFrom graphics abline arrows axis barplot box contour curve grid hist image layout legend lines matplot mtext pairs par persp plot points polygon rect segments symbols text title
 #' @importFrom kableExtra cell_spec kable kable_styling scroll_box
-#' @importFrom stats acf aggregate density dnorm loess
-#'   median na.omit nlminb optim pnorm qbeta qchisq qlnorm
-#'   qnorm qt quantile rnorm sd var window
-#' @importFrom utils browseURL flush.console head
-#'   packageDescription read.csv
-#'   read.table tail write.csv write.table
+#' @importFrom stats acf aggregate density dnorm loess median na.omit nlminb optim pnorm qbeta qchisq qlnorm qnorm qt quantile rnorm sd var window
+#' @importFrom utils browseURL flush.console head packageDescription read.csv read.table tail write.csv write.table
 #'
 #' @examples
 #' \dontrun{
